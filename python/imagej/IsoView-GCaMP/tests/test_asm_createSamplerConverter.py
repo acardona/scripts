f853ce4f080af784a64c04853afd214150aa3bac
Albert Cardona <sapristi@gmail.com>
2018-11-24 23:37:53 -0500

    test asm createSamplerConverter: comparison with Weaver.method.
    The Weaver works as fast as the ASM version, in the best case,
    but the max time is about twice as large: saving .java files to disk
    and compiling them to .class files and then loading them has a lot
    of overhead compared with ASM's in-memory approach.

--- python/imagej/IsoView-GCaMP/tests/test_asm_createSamplerConverter.py
+++ python/imagej/IsoView-GCaMP/tests/test_asm_createSamplerConverter.py
@@ -17,6 +17,8 @@ from net.imglib2.util import ImgUtil
 from net.imglib2.img import ImgView
 from net.imglib2.img.basictypeaccess import FloatAccess
 from net.imglib2.converter.readwrite import SamplerConverter
+from net.imglib2 import Sampler
+from fiji.scripting import Weaver
 
 dimensions = [100, 100, 100]
 img1 = ArrayImgs.unsignedBytes(dimensions)
@@ -47,5 +49,33 @@ def testJython():
 
 timeit(20, testJython)
 
+
+w = Weaver.method("""
+public SamplerConverter makeUnsignedByteToFloatSamplerConverter() {
+  return new SamplerConverter() {
+    public final FloatType convert(final Sampler sampler) {
+      return new FloatType(new FloatAccess() {
+        final private Sampler sampler_ = sampler;
+        final public float getValue(final int index) {
+          final UnsignedByteType t = (UnsignedByteType)this.sampler_.get();
+          return t.getRealFloat();
+        }
+        final public void setValue(final int index, final float value) {
+          final UnsignedByteType t = (UnsignedByteType)this.sampler_.get();
+          t.setReal(value);
+        }
+      });
+    }
+  };
+}
+""", [SamplerConverter, Sampler, FloatType, FloatAccess, UnsignedByteType])
+
+def testWeaver():
+  ImgUtil.copy(ImgView.wrap(Converters.convertRandomAccessibleIterableInterval(img1, w.makeUnsignedByteToFloatSamplerConverter()), img1.factory()), img2)
+
+timeit(20, testWeaver)
+
 # ASM: 31 ms
 # Jython: 755 ms   -- a factor of 25x or so slowdown
+# Weaver: 31 ms -- but the max is twice as large: saving files to disk and compiling them
+#                  to .class files in disk is expensive. ASM initial cost is far smaller.
