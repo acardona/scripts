c4349d320a1836fe180fd0c62134a8b3b7ead862
Albert Cardona <sapristi@gmail.com>
2019-01-16 13:56:53 -0500

    program register+deconvolve_isoview_4D_series.py: * disabled parameters for registration intra-timepoint. These are now   expected as matrices, like those obtained by the load_four_cameras...check.py * reduced deconvolution iterations to 5 and 7 for CM00+CM01, CM02+CM03 respectively. * blockSize is now the whole image. Most performant for both CPU and GPU usage.

--- python/imagej/IsoView-GCaMP/programs/register+deconvolve_isoview_4D_series.py
+++ python/imagej/IsoView-GCaMP/programs/register+deconvolve_isoview_4D_series.py
@@ -1,18 +1,18 @@
 import sys
-sys.path.append("/groups/cardona/home/cardonaa/lab/scripts/python/imagej/IsoView-GCaMP/")
+sys.path.append("//home/albert/lab/scripts/python/imagej/IsoView-GCaMP/")
 from lib.isoview import deconvolveTimePoints
 from mpicbg.models import RigidModel3D
 
 
 # The folder with the sequence of TM\d+ folders, one per time point in the 4D series.
 # Each folder should contain 4 KLB files, one per camera view of the IsoView microscope.
-srcDir = "/groups/zlatic/zlaticlab/Nadine/Raghav/2017-05-10/GCaMP6s_1_20170510_115003.corrected/SPM00/"
+srcDir = "/home/albert/shares/zlaticlab/Nadine/Raghav/2017-05-10/GCaMP6s_1_20170510_115003.corrected/SPM00/"
 
 # A folder to save deconvolved images in, and CSV files describing features, point matches and transformations
-targetDir = "/groups/cardona/cardonalab/Albert/2017-05-10_1018/"
+targetDir = "/home/albert/shares/cardonalab/Albert/2017-05-10_1018/"
 
 # Path to the volume describing the point spread function (PSF)
-kernelPath = "/groups/cardona/cardonalab/Albert/Raghav-IsoView-PSF/PSF-19x19x25.tif"
+kernelPath = "/home/albert/shares/cardonalab/Albert/Raghav-IsoView-PSF/PSF-19x19x25.tif"
 
 # The calibration is [0.40625, 0.40625, 2.03125]
 # To preserve XY pixels, expand Z only:
@@ -36,6 +36,7 @@ def cameraTransformations(img1, img2, img3, img4, calibration):
   }
 
 
+"""
 # Parameters for DoG difference of Gaussian to detect soma positions
 somaDiameter = 8 * calibration[0]
 paramsDoG = {
@@ -54,7 +55,7 @@ paramsFeatures = {
   "angle_epsilon": 0.02, # in radians. 0.05 is 2.8 degrees, 0.02 is 1.1 degrees
   "len_epsilon_sq": pow(somaDiameter, 2), # in calibrated units, squared
   "pointmatches_nearby": 1, # if 1 (True), searches for possible matches only within radius
-  "pointmatches_search_radius": somaDiameter * 2 #
+  "pointmatches_search_radius": somaDiameter * 2
 }
 
 # RANSAC parameters: reduce list of pointmatches to a spatially coherent subset
@@ -65,19 +66,20 @@ paramsModel = {
   "n_iterations": 2000, # for estimating the model
   "maxTrust": 4, # for rejecting candidates
 }
+"""
 
 # Deconvolution parameters
 paramsDeconvolution = {
-  "blockSize": [128, 128, 128],
-  "CM_0_1_n_iterations": 10,
-  "CM_2_3_n_iterations": 30,
+  "blockSize": None, # None means the image size. Otherwise specify like e.g. [128, 128, 128]
+  "CM_0_1_n_iterations": 5,
+  "CM_2_3_n_iterations": 7,
 }
 
 # Joint dictionary of parameters
 params = {}
-params.update(paramsDoG)
-params.update(paramsFeatures)
-params.update(paramsModel)
+#params.update(paramsDoG)
+#params.update(paramsFeatures)
+#params.update(paramsModel)
 params.update(paramsDeconvolution)
 
 
@@ -85,12 +87,41 @@ params.update(paramsDeconvolution)
 roi = ([1, 228, 0], # top-left coordinates
        [1 + 406 -1, 228 + 465 -1, 0 + 325 -1]) # bottom-right coordinates (inclusive, hence the -1)
 
+# After cropping, these are the finer transformations from any camera view to CM00.
+# (discovered with the script: load_four_cameras_single_timepoint_transformed_registration-check.py)
+# These registrations don't change from timepoint to timepoint within the same time series acquisition.
+"""
+fineTransformsPostROICrop = {
+    "CM00-CM01": [0.9999949529841275, -0.0031770224721305684, 2.3118912942710207e-05, -1.6032353998500826,
+                  0.003177032139125933, 0.999994860398559, -0.00043086338151948394, -0.4401520585103873,
+                  -2.1749931475206362e-05, 0.0004309346564745992, 0.9999999069111268, 6.543187040788581],
+    "CM02-CM03": [0.999924841032604, -0.01217657381949277, 0.0014294530211431203, 5.000564809506895,
+                  0.012172248426589044, 0.9999214253386473, 0.0029965842169644608, -5.177094692032659,
+                  -0.001465828831280259, -0.002978959339501507, 0.9999944885583574, -0.7804321574068354]
+    }
+"""
+# All 4 cameras relative to CM00
+fineTransformsPostROICrop = \
+   [[1, 0, 0, 0,
+     0, 1, 0, 0,
+     0, 0, 1, 0],
+    [0.9999949529841275, -0.0031770224721305684, 2.3118912942710207e-05, -1.6032353998500826,
+     0.003177032139125933, 0.999994860398559, -0.00043086338151948394, -0.4401520585103873,
+     -2.1749931475206362e-05, 0.0004309346564745992, 0.9999999069111268, 6.543187040788581],
+    [0.9997987121628504, -0.009472768268010913, -0.01768620419553878, 3.930297652126247,
+     0.009342009169030474, 0.9999285252136545, -0.007461322183871208, 2.427195709390503,
+     0.017755619453893427, 0.0072945956287064715, 0.999815746451526, 1.0095040792330394],
+    [0.9998779568655723, -0.015226665797195312, 0.0034957149525624287, 3.2525448680408826,
+     0.01523705113687456, 0.9998795170641414, -0.002963718649331728, -2.1506102341571323,
+     -0.0034501662251717287, 0.003016621335310332, 0.9999894981192247, 2.447694931285838]]
+
 
 # The transformation model for registering views onto each other
 modelclass = RigidModel3D
 
 deconvolveTimePoints(srcDir, targetDir, kernelPath, calibration,
-                    cameraTransformations, params, modelclass, roi, subrange=range(0, 10))
+                    cameraTransformations, fineTransformsPostROICrop,
+                    params, roi, subrange=range(0, 1))
 
 
 
