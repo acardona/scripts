241335d15387fada4cd1b5a335ec7ab16e229509
Albert Cardona <sapristi@gmail.com>
2019-02-07 12:50:20 -0500

    lib/synthetic.py: use native ASM-based RadiusBounds class
    Should speed up the "get" method a lot.

--- python/imagej/IsoView-GCaMP/lib/synthetic.py
+++ python/imagej/IsoView-GCaMP/lib/synthetic.py
@@ -2,24 +2,26 @@ from net.imglib2.view import Views
 from net.imglib2 import RealPoint, RealRandomAccess, KDTree, RealRandomAccessible
 from net.imglib2.neighborsearch import NearestNeighborSearchOnKDTree
 from net.imglib2.type.numeric.integer import UnsignedByteType
+from synthetic_asm import makeNativeRadiusBounds
 
+RadiusBounds = makeNativeRadiusBounds()
 
-class RadiusBounds(RealPoint, RealRandomAccess):
-  """ The definition of a point with a diameter in space. """
-  def __init__(self, n_dimensions, kdtree, radius, inside, outside):
-    super(RealPoint, self).__init__(n_dimensions)
-    self.search = NearestNeighborSearchOnKDTree(kdtree)
-    self.radius = radius
-    self.radius_squared = radius * radius
-    self.inside = inside
-    self.outside = outside
-  def copyRealRandomAccess(self):
-    return RadiusBounds(self.numDimensions(), self.kdtree, self.radius, self.inside, self.outside)
-  def get(self):
-    self.search.search(self)
-    if self.search.getSquareDistance() < self.radius_squared:
-      return self.inside
-    return self.outside
+#class RadiusBounds(RealPoint, RealRandomAccess):
+#  """ The definition of a point with a diameter in space. """
+#  def __init__(self, n_dimensions, kdtree, radius, inside, outside):
+#    super(RealPoint, self).__init__(n_dimensions)
+#    self.search = NearestNeighborSearchOnKDTree(kdtree)
+#    self.radius = radius
+#    self.radius_squared = radius * radius
+#    self.inside = inside
+#    self.outside = outside
+#  def copyRealRandomAccess(self):
+#    return RadiusBounds(self.numDimensions(), self.kdtree, self.radius, self.inside, self.outside)
+#  def get(self):
+#    self.search.search(self)
+#    if self.search.getSquareDistance() < self.radius_squared:
+#      return self.inside
+#    return self.outside
 
 class RadiusBoundsRealRandomAccessible(RealRandomAccessible):
   """ The RealRandomAccessible that wraps the RadiusBounds in space, unbounded.
