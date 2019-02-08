d21f11bfc6ac497dcf85408b0b77b4e32308c5f9
Albert Cardona <sapristi@gmail.com>
2019-02-08 17:20:22 -0500

    programs/deltaF_over_F.py: still unfinished, shows detected nuclei as a composite image over the max projection (4D to 3D).

--- python/imagej/IsoView-GCaMP/programs/deltaF_over_F.py
+++ python/imagej/IsoView-GCaMP/programs/deltaF_over_F.py
@@ -3,10 +3,12 @@ sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP/")
 from lib.io import readN5
 from lib.dogpeaks import createDoG
 from lib.synthetic import virtualPointsRAI
-from lib.ui import showStack
-from lib.nuclei import findPeaks, mergePeaks, filterNuclei
+from lib.ui import showStack, wrap, showAsComposite
+from lib.nuclei import findPeaks, mergePeaks, filterNuclei, findNucleiByMaxProjection
 from net.imglib2.util import Intervals
 from net.imglib2.view import Views
+from ij import IJ
+from net.imglib2.img.display.imagej import ImageJFunctions as IL
 
 n5dir = "/home/albert/shares/cardonalab/Albert/2017-05-10_1018/deconvolved/n5"
 dataset_name = "2017-5-10_1018_0-399_X203_Y155_Z65"
@@ -20,8 +22,8 @@ img4Da = Views.subsample(img4D,
 img4Db = Views.subsample(Views.interval(img4D, [0, 0, 0, 1], Intervals.maxAsLongArray(img4D)),
                          [1, 1, 1, 2]) # step
 
-showStack(img4Da, title="CM00+CM01 registered+deconvolved")
-showStack(img4Db, title="CM02+CM03 registered+deconvolved")
+#showStack(img4Da, title="CM00+CM01 registered+deconvolved")
+#showStack(img4Db, title="CM02+CM03 registered+deconvolved")
 
 calibration = [1.0, 1.0, 1.0]
 somaDiameter = 8 * calibration[0]
@@ -30,7 +32,7 @@ params = {
  "frames": 5, # number of time frames to average, 5 is equivalent to 3.75 seconds: 0.75 * 5
  "calibration": calibration, # Deconvolved images have isotropic calibration
  "somaDiameter": somaDiameter, # in pixels
- "minPeakValue": 40, # determined by hand: the bright peaks
+ "minPeakValue": 50, # determined by hand: the bright peaks
  "sigmaSmaller": somaDiameter / 4.0, # in calibrated units: 1/4 soma
  "sigmaLarger": somaDiameter / 2.0,  # in calibrated units: 1/2 soma
  "searchRadius": somaDiameter / 3.0,
@@ -43,6 +45,7 @@ params = {
 #img4Da = Views.interval(img4Da, FinalInterval([img4Da.dimension(0), img4Da.dimension(1), img4Da.dimension(2), 5]))
 
 
+"""
 peaks = findPeaks(img4Da, params)
 mergedPeaks = mergePeaks(peaks, params)
 nuclei = filterNuclei(mergedPeaks, params)
@@ -50,4 +53,11 @@ nuclei = filterNuclei(mergedPeaks, params)
 # Show as a 3D volume with spheres
 spheresRAI = virtualPointsRAI(nuclei, somaDiameter / 2.0, Views.hyperSlice(img4D, 3, 1))
 imp = showStack(spheresRAI, title="nuclei (min_count=%i)" % params["min_count"])
+"""
 
+
+# Using a ready-made max projection for testing
+impMax = IJ.getImage()
+img3D = IL.wrap(impMax)
+peaks, spheresRAI, impSpheres = findNucleiByMaxProjection(img4D, params, img3D=img3D)
+comp = showAsComposite([impMax, impSpheres])
