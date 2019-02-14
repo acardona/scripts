6d54885d9f78ca56788f8b6f885bff68559b9965
Albert Cardona <sapristi@gmail.com>
2019-02-14 10:00:04 -0500

    util/view_deltaFoF_nuclei_in_4D.py: fix errors.

--- python/imagej/IsoView-GCaMP/util/view_deltaFoF_nuclei_in_4D.py
+++ python/imagej/IsoView-GCaMP/util/view_deltaFoF_nuclei_in_4D.py
@@ -4,12 +4,16 @@ sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP/")
 from lib.synthetic import virtualPointsRAI
 from lib.ui import wrap
 from ij3d import Image3DUniverse
-from itertools import islice
+from itertools import islice, imap
+from net.imglib2 import FinalInterval, RealPoint
+from net.imglib2.view import Views
+from net.imglib2.type.numeric.integer import UnsignedByteType
+from ij import CompositeImage
 
 
 baseDir = "/home/albert/shares/cardonalab/Albert/2017-05-10_1018/"
 # A file whose header has the points as "x::y::z"
-csvFilename"deconvolved/CM00-CM01_deltaFoF.csv"
+csvFilename = "deconvolved/CM00-CM01_deltaFoF.csv"
 
 somaDiameter = 8 # pixels
 radius = somaDiameter / 2.0
@@ -19,15 +23,19 @@ maximum = 9.4
 span = maximum - minimum
 
 def to8bitRange(values):
-  return [int((min(max(val, minimum), maximum) / span) * 255) for val in values]
+  return [UnsignedByteType(int((min(max(val, minimum), maximum) / span) * 255)) for val in values]
 
 with open(os.path.join(baseDir, csvFilename), 'r') as csvfile:
   reader = csv.reader(csvfile, delimiter=',', quotechar='"')
   header = reader.next()
-  peaks = [RealPeak.wrap(imap(float, peak.split('::'))) for peak in islice(1, header, None)]
-  frames = [virtualPointsRAI(peaks, radius, interval, inside=to8bitRange(map(float, islice(1, row, None)))) for row in reader]
+  peaks = [RealPoint.wrap(imap(float, peak.split('::'))) for peak in islice(header, 1, None)]
+  frames = [virtualPointsRAI(peaks, radius, interval, inside=to8bitRange(map(float, islice(row, 1, None)))) for row in reader]
   img4D = Views.stack(frames)
-  imp4D = wrap(img4D)
+  imp4D = wrap(img4D, "deltaF/F")
+  imp4D.setDimensions(1, img4D.dimension(2), img4D.dimension(3))
+  imp4D.setDisplayRange(0, 255)
+  com = CompositeImage(imp4D, CompositeImage.GRAYSCALE)
+  com.show()
   univ = Image3DUniverse(512, 512)
   univ.show()
-  univ.addVoltex(imp4D)
+  univ.addVoltex(com)
