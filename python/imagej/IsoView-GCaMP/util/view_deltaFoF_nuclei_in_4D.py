6643f2bf1b293d2aefce534bf32fcec1ebba24b1
Albert Cardona <sapristi@gmail.com>
2019-02-15 14:22:07 -0500

    util/view_deltaFoF_nuclei_in_4D.py: solved remaining issues.
    Major one was that the values have to be rezeroed prior to dividing by span.

--- python/imagej/IsoView-GCaMP/util/view_deltaFoF_nuclei_in_4D.py
+++ python/imagej/IsoView-GCaMP/util/view_deltaFoF_nuclei_in_4D.py
@@ -2,7 +2,7 @@ from __future__ import with_statement
 import sys, os, csv
 sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP/")
 from lib.synthetic import virtualPointsRAI
-from ij3d import Image3DUniverse, Content, ContentCreator
+from ij3d import Image3DUniverse, Content, ContentInstant
 from customnode import CustomMultiMesh, MeshMaker, CustomTriangleMesh
 from org.scijava.vecmath import Color3f
 from itertools import islice, imap, izip
@@ -11,6 +11,7 @@ from net.imglib2.view import Views
 from net.imglib2.type.numeric.integer import UnsignedByteType
 from net.imglib2.img.display.imagej import ImageJVirtualStackUnsignedByte
 from ij import ImagePlus, CompositeImage
+from java.util import TreeMap
 
 
 baseDir = "/home/albert/shares/cardonalab/Albert/2017-05-10_1018/"
@@ -26,7 +27,8 @@ span = maximum - minimum
 range_max = 255 # should be 255
 
 def to8bitRange(values):
-  return [UnsignedByteType(int((min(max(val, minimum), maximum) / span) * range_max)) for val in values]
+  # Ensure the value is inside [minimum, maximum] range, then rezero by subtracting minimum, and divide by span (maximum - minimum)
+  return [UnsignedByteType(int(((min(max(val, minimum), maximum) - minimum) / span) * range_max)) for val in values]
 
 def withVirtualStack(time_window=None, subsample=None):
   with open(os.path.join(baseDir, csvFilename), 'r') as csvfile:
@@ -58,19 +60,29 @@ def withIcospheres(time_window=None):
     peaks = [RealPoint.wrap(imap(float, peak.split('::'))) for peak in islice(header, 1, None)]
     # Template icosahedron
     ico = MeshMaker.createIcosahedron(2, radius)
-    # Share meshes across all timepoints
+    # Share lists of Point3f across all timepoints
     icos = [MeshMaker.copyTranslated(ico, peak.getFloatPosition(0), peak.getFloatPosition(1) , peak.getFloatPosition(2)) for peak in peaks]
     #
     univ = Image3DUniverse(512, 512)
+    instants = TreeMap()
     #
-    #rows = reader if time_window is None else islice(reader, time_window[0], time_window[1])
-    for row in reader:
+    rows = reader if time_window is None else islice(reader, time_window[0], time_window[1])
+    for row in rows:
+      # Values mapped to 0-1: Color3f takes 3 values in domain [0, 1].
+      # So: ensure the value is inside [minimum, maximum] range, then rezero by subtracting minimum, and divide by span (maximum - minimum)
+      values = ((min(max(float(v), minimum), maximum) - minimum) / span for v in islice(row, 1, None))
       meshes = [CustomTriangleMesh(mesh, Color3f(v, v, v), 0)
-                for v, mesh in izip(imap(lambda val: (min(max(val, minimum), maximum) / span), islice(reader, 1, None)), icos)]
+                for v, mesh in izip(values, icos)]
+      ci = ContentInstant(str(row[0]))
+      ci.display(CustomMultiMesh(meshes)) # each mesh has its color
+      ci.setLocked(True)
       print row[0]
-      content = ContentCreator.createContent(CustomMultiMesh(meshes), str(row[0]), int(row[0])) # sets the timepoint
-      univ.addContentLater(content)
+      instants.put(int(row[0]), ci)
+    
+    print "n instants:", instants.size()
+    univ.addContent(Content("deltaF/F", instants, False))
     univ.show()
+    univ.updateStartAndEndTime(0, len(instants) -1)
 
 
-withIcospheres(time_window=(0, 10))
+withIcospheres(time_window=(0, 100))
