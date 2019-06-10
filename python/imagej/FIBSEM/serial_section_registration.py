ea95c786d6bc4f74ab11826642807ee63546cb5f
Albert Cardona <sapristi@gmail.com>
2019-06-10 16:05:26 -0400

    FIBSEM/serial_section_registration.py: don't use compression for N5 export.
    The file size is within 5% of the raw one.

--- python/imagej/FIBSEM/serial_section_registration.py
+++ python/imagej/FIBSEM/serial_section_registration.py
@@ -570,6 +570,7 @@ exportDir = "/groups/cardona/cardonalab/FIBSEM_L1116_exports/n5/"
 interval = FinalInterval([864, 264], [864 + 15312 -1, 264 + 17424 -1])
 
 
+# Don't use compression: less than 5% gain, at considerable processing cost.
 export8bitN5(filepaths, dimensions, loadMatrices("matrices", csvDir),
-             name, exportDir, interval, gzip_compression=6, block_size=[256,256,32], # ~2 MB per block
+             name, exportDir, interval, gzip_compression=0, block_size=[256,256,32], # ~2 MB per block
              n5_threads=0)
