5a44cf3a1723aef5bfa372e0d7fc0e6c18a0fcb4
Albert Cardona <sapristi@gmail.com>
2019-06-11 11:31:35 -0400

    FIBSEM/serial_section_registration.py: preloader prints less, only when it actually has to preload anything. Otherwise silent.

--- python/imagej/FIBSEM/serial_section_registration.py
+++ python/imagej/FIBSEM/serial_section_registration.py
@@ -532,7 +532,7 @@ def export8bitN5(filepaths,
         return
       first = keys[-1] - (keys[-1] % block_size[2])
       keys = None
-      syncPrint("Preloading %i-%i" % (first, first + block_size[2] -1))
+      msg = "Preloading %i-%i" % (first, first + block_size[2] -1)
       futures = []
       for index in xrange(first, first + block_size[2]):
         futures.append(exe.submit(TimeItTask(softCache.get, index, loader)))
@@ -543,9 +543,13 @@ def export8bitN5(filepaths,
         r, t = futures.pop(0).get()
         # t in miliseconds
         if t > 500:
+          if msg:
+            syncPrint(msg)
+            msg = None
           syncPrint("preloaded index %i in %f ms" % (first + count, t))
-        count += 1        
-      syncPrint("Completed preloading %i-%i" % (first, first + block_size[2] -1))
+        count += 1
+      if not msg: # msg was printed
+        syncPrint("Completed preloading %i-%i" % (first, first + block_size[2] -1))
     except:
       syncPrint(sys.exc_info())
     finally:
