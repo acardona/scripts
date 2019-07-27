481d9422a5593b13c46e350e2dc043c5130965ec
Albert Cardona <sapristi@gmail.com>
2019-07-26 23:18:04 -0400

    trakem2_view_N5_volume.py: many fixes.
    NOTE you'll need a recent master of TrakEM2, because there was an error
    in how pathless images where generated with preprocessor scripts:
    the pointer to the new ImagePlus wasn't being returned from Patch.getImagePlus()
    the first time, even though the second and so on yes, because it was cached.

--- python/imagej/trakem2/trakem2_view_N5_volume.py
+++ python/imagej/trakem2/trakem2_view_N5_volume.py
@@ -8,6 +8,7 @@ from ini.trakem2.utils import Saver
 from ij import ImagePlus
 from java.awt import Color, Rectangle
 from java.awt.geom import AffineTransform
+from java.lang import Runtime
 
 
 # From the N5 attributes:
@@ -17,8 +18,12 @@ name = "FIBSEM_L1116"  # The name of the ImagePlus showing the N5 volume
 
 # CATMAID target folder for tiles
 tgt_dir = "/groups/cardona/cardonalab/" + name + "_CATMAID_tiles/"
-if not os.exists(tgt_dir):
+if not os.path.exists(tgt_dir):
   os.mkdir(tgt_dir)
+
+# CATMAID tile side
+tile_side = 1024
+  
 
 # Grab an empty project created from the Fiji GUI:
 project = Project.getProjects()[0]
@@ -29,6 +34,7 @@ project.getLoader().setMipMapsRegeneration(False)
 
 # Adjust first layer to Z of -1 (as proper layers will be added from 0 onwards
 layerset = project.getRootLayerSet()
+layerset.setDimensions(0, 0, dimensions[0], dimensions[1])
 layerset.getLayers().get(0).setZ(-1.0)
 # (This first layer serves as a layer to avoid repainting anything.)
 
@@ -39,11 +45,12 @@ import ij.ImagePlus;
 import ij.WindowManager;
 
 ids = WindowManager.getIDList();
-for (int i=0; i<ids.length; ++i) {
+for (i=0; i<ids.length; ++i) {
   src = WindowManager.getImage(ids[i]); // an ImagePlus
-  if (src.getStack().isVirtual() && src.getTitle() == %s) {
+  stack = src.getStack();
+  if (null != stack && stack.isVirtual() && "%s".equals(src.getTitle())) {
     layer = patch.getLayer();
-    ip = src.getStack().getProcessor(layer.getParent().getLayerIndex(layer) + 1); // 1-based
+    ip = stack.getProcessor(layer.getParent().getLayerIndex(layer.getId()) + 1); // 1-based
     imp.setProcessor("", ip);
     break;
   }
@@ -57,28 +64,30 @@ with open(preprocessor_script_path, 'w') as f:
 # Create as many layers as indices in the Z dimension
 layers = []
 for z in xrange(dimensions[2]):
-  layer = Layer(project, z, 1.0, layerset)
+  layer = layerset.getLayer(z, 1.0, True) # create if not there
   layerset.addSilently(layer)
   layers.append(layer)
+  # Add a single Patch instance per Layer, whose image is a 2D crop of the N5 volume
+  if 0 == layer.getDisplayables().size():
+    index = layerset.getLayerIndex(layer.getId())
+    patch = Patch(project, str(z), dimensions[0], dimensions[1],
+                  dimensions[0], dimensions[1],
+                  img_type, 1.0,
+                  Color.black, True,
+                  0, 255,
+                  AffineTransform(), "")
+    layer.add(patch, False) # don't update displays
+    project.getLoader().setPreprocessorScriptPathSilently(patch, preprocessor_script_path)
 
 layerset.recreateBuckets(layers, True)
 Display.updateLayerScroller(layerset)
 
-# Add a single Patch instance per Layer, whose image is a 2D crop of the N5 volume
-for layer in layerset.getLayers():
-  index = layerset.getLayerIndex(layer.getId())
-  patch = Patch(project, "", dimensions[0], dimensions[1],
-                dimensions[0], dimensions[1],
-                img_type, 1.0,
-                Color.black, True,
-                0, 255,
-                AffineTransform(), "")
-  patch.setPreprocessorScriptPathSilently(preprocessor_script_path)
-
-
 # Export for CATMAID from raw images (strategy=0)
+"""
 saver = Saver("jpg")
 saver.setQuality(0.75)
 ExportMultilevelTiles.makePrescaledTiles(layers, Patch, Rectangle(0, 0, dimensions[0], dimensions[1]),
-                                         -1, img_type, tgt_dir, 0, saver, True, True,
+                                         -1, img_type, tgt_dir, 0, saver, tile_side, 1,
+                                         True, True,
                                          Runtime.getRuntime().availableProcessors())
+"""
