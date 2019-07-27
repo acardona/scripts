d1da7991baee8869c29a2e491d069aff659b42e0
Albert Cardona <sapristi@gmail.com>
2019-07-26 23:14:47 -0400

    FIBSEM/view_exported_N5.py: view the whole volume.

--- python/imagej/FIBSEM/view_exported_N5.py
+++ python/imagej/FIBSEM/view_exported_N5.py
@@ -1,11 +1,13 @@
 import sys
-sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP/")
+sys.path.append("/groups/cardona/home/cardonaa/lab/scripts/python/imagej/IsoView-GCaMP/")
 from net.imglib2.view import Views
 from net.imglib2 import FinalInterval
 from lib.io import readN5
 from lib.ui import showStack
 
-img3D = readN5("/groups/cardona/cardonalab/FIBSEM_L1116_exports/n5/", "FIBSEM_L1116", show=None)
-fov = Views.interval(img3D, FinalInterval([4096, 4096, 0], [8192 -1, 8192 -1, 13770 -1]))
-imp = showStack(fov, title="test")
-imp.setPosition(imp.getStack().size()) # last slice
+name = "FIBSEM_L1116"
+img3D = readN5("/groups/cardona/cardonalab/FIBSEM_L1116_exports/n5/", name, show=None)
+#fov = Views.interval(img3D, FinalInterval([4096, 4096, 0], [8192 -1, 8192 -1, 13770 -1]))
+fov = img3D # whole
+imp = showStack(fov, title=name)
+#imp.setPosition(imp.getStack().size()) # last slice
