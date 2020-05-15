eb08739407ab0af89b1868bf0d31fa97aba1296a
Albert Cardona <sapristi@gmail.com>
2020-05-15 15:39:50 +0100

    example datastructure deque: showcase extendleft by creating a stack mirroring option which mirrors the stack from the current slice to the end by appending at the beginning the reversed list of slices from current slice to end.

--- python/imagej/examples/example_datastructure_deque.py
+++ python/imagej/examples/example_datastructure_deque.py
@@ -2,8 +2,8 @@
 from collections import deque
 from ij import IJ, ImagePlus, VirtualStack
 from java.awt.event import KeyAdapter
-from java.awt.event.KeyEvent import VK_LEFT, VK_RIGHT, VK_UP, VK_DOWN
-
+from java.awt.event.KeyEvent import VK_LEFT, VK_RIGHT, VK_UP, VK_DOWN, VK_M, VK_R, VK_W
+from itertools import islice
 
 class Shifter(KeyAdapter):
   # Shared across all Shifter instances
@@ -26,14 +26,27 @@ class Shifter(KeyAdapter):
 
   # On key pressed
   def keyPressed(self, event):
-    n = Shifter.moves.get(event.getKeyCode(), 0)
+    key = event.getKeyCode()
+    n = Shifter.moves.get(key, 0)
     if 0 != n:
       self.cstack.shiftSlicesBy(n)
-      # Doesn't work for stacks:
-      # self.imp.updateAndDraw()
-      # Use instead to update the rendered image:
-      self.imp.setStack(self.cstack)
       event.consume()
+    elif VK_R == key:
+      self.cstack.reset()
+      event.consume()
+    elif VK_M == key:
+      print 'M'
+      self.cstack.mirrorSlicesAt(self.imp.getCurrentSlice())
+      event.consume()
+    elif VK_W == key:
+      if not event.isControlDown(): # otherwise, left control+W close the window
+        width = IJ.getNumber("Window width:", min(7, self.cstack.size()))
+        if not (IJ.CANCELED == width):
+          self.cstack.windowAroundSlice(self.imp.getCurrentSlice(), width)
+        event.consume()
+    if event.isConsumed():
+      # Refresh
+      self.imp.setStack(self.cstack)
     else:
       # Run pre-existing key listeners
       for l in self.listeners.get(event.getSource(), []):
@@ -48,13 +61,37 @@ class CyclicStack(VirtualStack):
     super(VirtualStack, self).__init__(stack.getWidth(), stack.getHeight(), stack.size())
     self.stack = stack
     self.sliceIndices = deque(xrange(1, stack.size() + 1))
-
-  # Rotate the deque either by +1 or -1, and update the image
-  def shiftSlicesBy(self, n):
-    self.sliceIndices.rotate(n)
+    self.setBitDepth(stack.getBitDepth())
 
   def getProcessor(self, index):
     return self.stack.getProcessor(self.sliceIndices[index-1])
+
+  def getSize(self):
+    return len(self.sliceIndices)
+
+  def shiftSlicesBy(self, n):
+    # Rotate the deque either by +1 or -1, and update the image
+    self.sliceIndices.rotate(n)
+
+  def mirrorSlicesAt(self, slice_index): # slice_index is 1-based
+    # Remove slices from 0 to slice_index (exclusive), i.e. crop to from slice_index to the end
+    self.sliceIndices = deque(islice(self.sliceIndices, slice_index -1, None))
+    # Append at the begining, reversed, all slices after slice n (which is now at index 0 of the deque)
+    self.sliceIndices.extendleft(list(islice(self.sliceIndices, 1, None))) # copy into list
+    print self.sliceIndices
+
+  def reset(self):
+    self.sliceIndices = deque(xrange(1, self.stack.size() + 1))
+
+  def windowAroundSlice(self, slice_index, width): # slice_index is 1-based
+    if 0 == width % 2: # if width is an even number
+      width += 1 # must be odd: current slice remains at the center
+    # New indices
+    half = int(width / 2)
+    first = max(slice_index - half, 0)
+    last  = min(slice_index + half, len(self.sliceIndices))
+    print first, last
+    self.sliceIndices = deque(islice(self.sliceIndices, first, last + 1))
 
 
 imp = IJ.getImage() # a stack
