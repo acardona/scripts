ca909262506cf4512b818b8ac4bf7ee0f91eae53
Albert Cardona <sapristi@gmail.com>
2020-08-23 22:09:18 +0100

    Latest synthetic EM for learning a classifier of EM membrane boundaries

--- python/imagej/tests/test_synthetic_EM.py
+++ python/imagej/tests/test_synthetic_EM.py
@@ -17,11 +17,16 @@ rois = [OvalRoi(w/2 - w/4, h/2 - h/4, w/4, h/2), # stretched vertically
         OvalRoi(w/2 - w/18, h/2 + h/10, w/6, h/4),
         OvalRoi(w/2 - w/18, h/8 + h/16, w/3, h/5)]
 
-fp.setColor(0) # membrane
+fp.setColor(50) # membrane
 fp.setLineWidth(3)
 
 for roi in rois:
   fp.draw(roi)
+
+fp.setColor(90) # oblique membrane
+fp.setLineWidth(5)
+roi_oblique = OvalRoi(w/2 + w/8, h/2 + h/8, w/4, h/4)
+fp.draw(roi_oblique)
 
 # Add noise
 # 1. Vesicles
@@ -40,17 +45,28 @@ GaussianBlur().blurFloat(fp, sigma, sigma, 0.02)
 # 3. shot noise
 fp.noise(25.0)
 
+fp.setMinAndMax(0, 255)
 imp.show()
+
+# Try training for slightly off-center pixels from the drawed lines
+#ovals1 = [OvalRoi(r.x -1, r.y -1, r.width +1, r.height +1) for r in [oval.getBounds() for oval in rois]]
+#ovals2 = [OvalRoi(r.x +1, r.y +1, r.width -1, r.height -1) for r in [oval.getBounds() for oval in rois]]
 
 # Membrane, from the ovals: 312 points
 membrane = reduce(lambda cs, pol: [cs[0] + list(pol.xpoints), cs[1] + list(pol.ypoints)],
                   [roi.getPolygon() for roi in rois], [[], []])
 
+# Membrane oblique, fuzzy: another 76 points
+membrane_oblique = reduce(lambda cs, pol: [cs[0] + list(pol.xpoints), cs[1] + list(pol.ypoints)],
+                  [roi.getPolygon() for roi in [roi_oblique]], [[], []])
+
+len_membrane = len(membrane[0]) + len(membrane_oblique[0])
+
 # Background samples: as many as membrane samples
 rectangle = Roi(10, 10, w - 20, h - 20)
 pol = rectangle.getInterpolatedPolygon(1.0, False) # 433 points
-nonmem = (list(int(x) for x in pol.xpoints)[:len(membrane[0])],
-          list(int(y) for y in pol.ypoints)[:len(membrane[0])])
+nonmem = (list(int(x) for x in pol.xpoints)[:len_membrane],
+          list(int(y) for y in pol.ypoints)[:len_membrane])
 
 
 
@@ -58,23 +74,33 @@ nonmem = (list(int(x) for x in pol.xpoints)[:len(membrane[0])],
 
 import sys
 sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP/")
-from lib.features_em import createSMOClassifier, createRandomForestClassifier, classify
+from lib.segmentation_em import createSMOClassifier, createRandomForestClassifier, classify, \
+                                filterBank, filterBankRotations, filterBankBlockStatistics
 from net.imglib2.img.display.imagej import ImageJFunctions as IL
 from itertools import izip
 
 def samples():
-  for class_index, (xs, ys) in enumerate([membrane, nonmem]):
+  for class_index, (xs, ys) in enumerate([membrane, membrane_oblique, nonmem]):
     for x, y in izip(xs, ys):
       yield [x, y], class_index
 
+class_names = ["membranes", "mem_oblique", "nonmem"]
+
+img = IL.wrap(imp)
+
+ops = filterBank(img)
+angles = [0, 30] # range(0, 46, 9)
+#ops = filterBankRotations(img, angles=angles, bs=4, bl=8)
+#ops = filterBankBlockStatistics(img, block_width=5, block_height=5)
+
 # Create a classifier: support vector machine (SVM, an SMO in WEKA)
 # and save it for later
-classifierSMO = createSMOClassifier(IL.wrap(imp), samples(), ["membranes", "nonmem"],
+classifierSMO = createSMOClassifier(img, samples(), class_names, ops=ops,
                                     n_samples=len(membrane[0]), filepath="/tmp/svm-mem-nonmem")
 
 print classifierSMO.toString()
 
-classifierRF = createRandomForestClassifier(IL.wrap(imp), samples(), ["membranes", "nonmem"],
+classifierRF = createRandomForestClassifier(img, samples(), class_names, ops=ops,
                                             n_samples=len(membrane[0]), filepath="/tmp/rf-mem-nonmem")
 
 
@@ -85,16 +111,16 @@ impEM = WindowManager.getImage("180-220-sub512x512-30.tif") # IJ.getImage() # e.
 #impEM = IJ.openImage("/home/albert/lab/TEM/abd/microvolumes/Seg/180-220-sub/180-220-sub512x512-30.tif")
 imgEM = IL.wrap(impEM)
 
+ops = filterBank(imgEM)
+#ops = filterBankRotations(imgEM, angles=angles)
+#ops = filterBankBlockStatistics(imgEM, block_width=5, block_height=5)
+
 # Classify pixels as membrane or not
-resultSMO = classify(imgEM, "/tmp/svm-mem-nonmem", class_names=None)
-IL.wrap(resultSMO, "result SMO").show()
+resultSMO = classify(imgEM, classifierSMO, class_names, ops=ops)
+IL.wrap(resultSMO, "result SMO %s" % str(angles)).show()
 
-resultRF = classify(imgEM, "/tmp/rf-mem-nonmem", class_names=None)
-IL.wrap(resultRF, "result RF").show()
-
-
-
-
+resultRF = classify(imgEM, classifierRF, class_names, ops=ops, distribution_class_index=-1)
+IL.wrap(resultRF, "result RF %s" % str(angles)).show()
 
 
 
