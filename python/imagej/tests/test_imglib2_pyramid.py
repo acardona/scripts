20c3edb47419c9a4c392e4bc1cc2796333a0e1f0
Albert Cardona <sapristi@gmail.com>
2020-10-26 22:45:15 +0000

    Fixes to imglib2 pyramid

--- python/imagej/tests/test_imglib2_pyramid.py
+++ python/imagej/tests/test_imglib2_pyramid.py
@@ -1,28 +1,34 @@
 from net.imglib2.img.display.imagej import ImageJFunctions as IL
 from net.imglib2.view import Views
-from net.imglib2.realtransform import RealViews
+from net.imglib2.realtransform import RealViews, ScaleAndTranslation
 from net.imglib2.interpolation.randomaccess import NLinearInterpolatorFactory
-from net.imglib2.realtransform import Scale
+from net.imglib2.util import ImgUtil
 from net.imglib2 import FinalInterval
 from ij import IJ
 
 img = IL.wrap(IJ.getImage())
 
-imgR = Views.interpolate(Views.extendBorder(img), NLinearInterpolatorFactory())  
+pyramid = [img] # level 0 is the image itself
 
-# Create levels of a pyramid as interpolated views
+# Create levels of a pyramid with interpolation
 width = img.dimension(0)
 min_width = 32
-pyramid = [] # level 0 is the image itself, not added
-scale = 1.0
+
+s = [0.5 for d in xrange(img.numDimensions())]
+t = [-0.25 for d in xrange(img.numDimensions())]
 while width > min_width:
-  scale /= 2.0
   width /= 2
-  s = [scale for d in xrange(img.numDimensions())]
-  scaled = Views.interval(RealViews.transform(imgR, Scale(s)),
-                          FinalInterval([int(img.dimension(d) * scale)
-                                         for d in xrange(img.numDimensions())]))
-  pyramid.append(scaled)
+  imgE = Views.interpolate(Views.extendBorder(img), NLinearInterpolatorFactory())
+  # A scaled-down view of the imgR
+  level = Views.interval(RealViews.transform(imgE, ScaleAndTranslation(s, t)),
+                         FinalInterval([int(img.dimension(d) * 0.5)
+                                        for d in xrange(img.numDimensions())]))
+  # Create a new image for this level
+  scaledImg = img.factory().create(level) # of dimensions as of level
+  ImgUtil.copy(level, scaledImg) # copy the scaled down view into scaledImg
+  pyramid.append(scaledImg)
+  # Prepare for next iteration
+  img = scaledImg # for the dimensions of the level in the next iteration
 
 for i, imgScaled in enumerate(pyramid):
   IL.wrap(imgScaled, str(i+1)).show()
