a0a4cd76eec9c3eda3ed88daaf6075b62ae19230
Albert Cardona <sapristi@gmail.com>
2022-01-20 12:47:34 +0000

    test_lib_io_readFIBSEMdat.py

---
+++ python/imagej/IsoView-GCaMP/tests/test_lib_io_readFIBSEMdat.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP")
+from lib.io import readFIBSEMdat
+from net.imglib2.img.display.imagej import ImageJFunctions as IL
+
+path = "/home/albert/ark/raw/fibsem/pygmy-squid/2021-12_popeye/Popeye2/Y2021/M12/D23/FIBdeSEMAna_21-12-23_235849_0-0-0.dat"
+
+channels = readFIBSEMdat(path, channel_index=0)
+print channels
+IL.wrap(channels[0], "channel 1").show() # looks good
+#IL.wrap(channels[1], "channel 2").show() # looks grainy, lots of shot noise
+                                                                                                       
