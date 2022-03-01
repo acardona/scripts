00a273d4dffdd13a5779d25b2b919cd01fa62c46
Albert Cardona <sapristi@gmail.com>
2022-03-01 14:45:33 +0000

    Update compiler paths

--- java/asm/asmifier.sh
+++ java/asm/asmifier.sh
@@ -1,2 +1,2 @@
 # $1 is a .class file like #my/UnsignedByteToFloatSamplerConverter.class
-java -classpath /home/albert/Programming/fiji-new/Fiji.app/jars/imglib2-5.1.0.jar:/home/albert/Programming/fiji-new/Fiji.app/jars/asm-5.0.4.jar://home/albert/Programming/fiji-new/Fiji.app/jars/asm-util-5.0.4.jar org.objectweb.asm.util.ASMifier $1
+java -classpath /home/albert/software/Fiji.app/jars/imglib2-5.12.0.jar:/home/albert/software/Fiji.app/jars/asm-7.1.jar://home/albert/software/Fiji.app/jars/asm-util-7.1.jar org.objectweb.asm.util.ASMifier $1
