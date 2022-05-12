6ca3c1fc28ebe6cddea48eb7fa0bbfff3f4504db
Albert Cardona <sapristi@gmail.com>
2022-05-12 23:48:45 +0100

    Grouocho N5 view: load only an ROI of the brain for speedier browsing

--- python/imagej/FIBSEM/view_exported_N5_Groucho.py
+++ python/imagej/FIBSEM/view_exported_N5_Groucho.py
@@ -4,11 +4,28 @@ sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP/")
 from net.imglib2.view import Views
 from net.imglib2 import FinalInterval
 from lib.io import readN5
-from lib.ui import showStack
+from lib.ui import showStack, wrap
+from java.awt import Rectangle
 
 name = "Groucho"
 img3D = readN5("/home/albert/ark/raw/fibsem/pygmy-squid/2021-12_popeye/Popeye2/amphioxus/n5", name, show=None)
 #fov = Views.interval(img3D, FinalInterval([4096, 4096, 0], [8192 -1, 8192 -1, 13770 -1]))
 fov = img3D # whole
-imp = showStack(fov, title=name)
-#imp.setPosition(imp.getStack().size()) # last slice
+
+
+# UNCOMMENT to open the whole stack
+#imp = showStack(fov, title=name)
+
+
+# Directly open section 8031 and only for an ROI over a bit of the brain
+x = 11227
+y = 7897
+width = 3240
+height = 3320
+fov = Views.interval(img3D, FinalInterval([x, y, 0], # min coords
+                                          [x + width - 1, # max coords
+                                           y + height -1,
+                                           img3D.dimension(2) -1]))
+imp = wrap(fov, title="Small ROI in the Groucho brain", n_channels=1)
+imp.setPosition(8031) # in the middle of a 64-section block
+imp.show()
