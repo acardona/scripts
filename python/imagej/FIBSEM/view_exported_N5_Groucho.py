1382d59fc4485124d907aa2cb7332db0ad9d60ba
Albert Cardona <sapristi@gmail.com>
2022-05-13 15:52:26 +0100

    Better navigate Groucho N5 volume.

--- python/imagej/FIBSEM/view_exported_N5_Groucho.py
+++ python/imagej/FIBSEM/view_exported_N5_Groucho.py
@@ -4,7 +4,7 @@ sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP/")
 from net.imglib2.view import Views
 from net.imglib2 import FinalInterval
 from lib.io import readN5
-from lib.ui import showStack, wrap
+from lib.ui import showStack, wrap, navigate2DROI
 from java.awt import Rectangle
 
 name = "Groucho"
@@ -15,6 +15,7 @@ fov = img3D # whole
 
 # UNCOMMENT to open the whole stack
 #imp = showStack(fov, title=name)
+#imp.show()
 
 
 # Directly open section 8031 and only for an ROI over a bit of the brain
@@ -22,6 +23,7 @@ x = 11227
 y = 7897
 width = 3240
 height = 3320
+"""
 fov = Views.interval(img3D, FinalInterval([x, y, 0], # min coords
                                           [x + width - 1, # max coords
                                            y + height -1,
@@ -29,3 +31,12 @@ fov = Views.interval(img3D, FinalInterval([x, y, 0], # min coords
 imp = wrap(fov, title="Small ROI in the Groucho brain", n_channels=1)
 imp.setPosition(8031) # in the middle of a 64-section block
 imp.show()
+"""
+
+# Better: navigatable with arrows in X,Y and < > in Z
+impN = navigate2DROI(img3D,
+                     FinalInterval([x, y], # min coords
+                                   [x + width - 1, # max coords
+                                    y + height -1]),
+                     indexZ=8030) # section shown
+
