b78236b1736734f78fcd32d80d9bb2160dc4ec4e
Albert Cardona <sapristi@gmail.com>
2023-04-12 14:17:32 +0100

    test_xStripes_xlib_.jar.py: finally works thanks to Beat Munch, the author of xStripes plugin

--- python/imagej/tests/test_xStripes_xlib_.jar.py
+++ python/imagej/tests/test_xStripes_xlib_.jar.py
@@ -1,8 +1,7 @@
 from ij import IJ, ImagePlus
 from xjava.filter import xStripes
 from jarray import zeros
-from java.lang import System
-from ij.process import ShortProcessor
+from ij.process import ShortProcessor, ByteProcessor
 from net.imglib2.img.array import ArrayImgs
 from net.imglib2.img.basictypeaccess.array import ByteArray, IntArray
 from net.imglib2.algorithm.math import ImgMath
@@ -26,15 +25,15 @@ ba = ArrayImgs.unsignedBytes(ByteArray(bytes), [width, height])
 bi = ArrayImgs.unsignedInts(IntArray(intPixels), [width, height])
 ImgMath.compute(ImgMath.img(ba)).into(bi)
 
-numBits = 32
-numBands = 1 # wild guess
+numBits = 8
+numBands = 1 # means: single-channel image, not e.g., RGB with 3 bands
 waveletTyp = "db15"
-decId = [0, 1, 2, 3, 4, 5] # 0:5
-filtWid = 1 # tolerance?
-filtTyp = 'f' # FFT
-wavBord = "sym" # symmetrical mirroring
-horVer = 2 # both
-normConstr = ' ' # nothing
+decId = [0, 1, 2, 3, 4, 5] # decomposition 0:5
+filtWid = 5.0 # filter width (the damping coefficient defining the width of the Fourier filter)
+filtTyp = 'f' # f: FFT; a: subtract average; 'z': set to zero 
+wavBord = "sym" # symmetrical mirroring of the edges
+horVer = 2 # 0: filter horizontal stripes; 1: filter vertical stripes; 2: filter both
+normConstr = 'c' # 'c': constraint output values to numBits; 'n': normalize; ' ': do nothing
 doDispl = False # don't show results
 ints = xStripes.waveletFilt(width, height,
                             intPixels,
@@ -44,9 +43,16 @@ ints = xStripes.waveletFilt(width, height,
                             filtWid, filtTyp,
                             wavBord, horVer,
                             normConstr, doDispl)
+bytesFiltered = zeros(width * height, 'b')
+bfa = ArrayImgs.unsignedBytes(ByteArray(bytesFiltered), [width, height])
+bfi = ArrayImgs.unsignedInts(IntArray(ints), [width, height])
+ImgMath.compute(ImgMath.img(bfi)).into(bfa) # crop ints to byte
 
-filtered = ArrayImgs.unsignedInts(IntArray(ints), [width, height])
-ImageJFunctions.wrap(filtered, "xStripes-filtered").show()
+impFiltered = ImagePlus("filtered", ByteProcessor(width, height, bytesFiltered, None))
+impFiltered.show()
+
+#filtered = ArrayImgs.unsignedInts(IntArray(ints), [width, height])
+#ImageJFunctions.wrap(filtered, "xStripes-filtered").show()
 
                          
                          
