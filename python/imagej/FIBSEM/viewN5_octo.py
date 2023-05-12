3350caeb6de57c9f7b565544583024d7d0f37dfa
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2023-05-12 14:34:50 +0100

    Small changes to viewN5_octo.py

--- python/imagej/FIBSEM/viewN5_octo.py
+++ python/imagej/FIBSEM/viewN5_octo.py
@@ -14,14 +14,17 @@
 #       and most likely also a net.imglib2.img.cell.CellImg or net.imglib2.img.array.ArrayImg
 
 import sys
-sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP")
+#sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP")
+sys.path.append("/lmb/home/acardona/lab/scripts/python/imagej/IsoView-GCaMP/")
 from lib.io import readN5, writeN5
-from lib.ui import showStack
+from lib.ui import showStack, wrap
 from net.imglib2.view import Views
 from net.imglib2.img.display.imagej import ImageJFunctions as IL
+from ij import ImagePlus
 
 # Path to the parent directory of the N5 volume
-n5dir = "/home/albert/catsop-server/volumes/fibsem/all-larvae.n5/1120/v0/raw/"
+#n5dir = "/home/albert/catsop-server/volumes/fibsem/all-larvae.n5/1120/v0/raw/"
+n5dir = "/net/zstore1/achampion/alignment/output/1120/v0/n5/FIBSEM_L1120_FullCNS_8x8x8nm/"
 
 name = "s0"  # Name of the N5 volume
 
@@ -29,20 +32,26 @@ name = "s0"  # Name of the N5 volume
 #img = readN5(n5dir, name, show="IJ")
 
 # To open the whole N5 volume with the BigDataViewer:
-#img = readN5(n5dir, name, show="BDV")
+img = readN5(n5dir, name, show=None)
+imp = wrap(img, title="octo")
+imp.setSlice(3000)
+imp2 = ImagePlus("octo slice 3000", imp.getProcessor())
+imp2.show()
 
 # To open only a subvolume of the N5 volume:
+"""
 img = readN5(n5dir, name, show=None)
 minCoords = [12029, 10063, 4495]
 maxCoords = [13724, 11455, 5120]
 view = Views.interval(img, minCoords, maxCoords)
 imp = showStack(Views.zeroMin(view), title="octo_AL_crop")
-
+"""
 
 # To write the subvolume as N5, do:
+"""
 targetN5dir = "/home/albert/Desktop/t2/"
 name_subvolume = name + "-" + "_".join("%s%s" % (c, l) for c, l in zip(minCoords + maxCoords, "xyzxyz"))
 block_size = [256, 256, 256]
 gzip_compression_level = 0 # from 0 (none) to 9 (max), often 4 is a good trade off in speed vs amount of de/compression
 writeN5(view, targetN5dir, name_subvolume, block_size, gzip_compression_level=gzip_compression_level)
-
+"""
