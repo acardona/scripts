6347771f411db477d8ce05c3df1fdc2cfc102e92
Albert Cardona <sapristi@gmail.com>
2023-10-20 17:03:39 +0100

    Fix lots of errors in serial_section_registration_Pedro_Parker.py
    Still not done.

--- python/imagej/FIBSEM/serial_section_registration_Pedro_Parker.py
+++ python/imagej/FIBSEM/serial_section_registration_Pedro_Parker.py
@@ -1,7 +1,9 @@
+from __future__ import with_statement
 import sys, os
 sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP/")
+from lib.registration import saveMatrices, loadMatrices
 from lib.serial2Dregistration import ensureSIFTFeatures
-from lib.io import findFilePaths, readFIBSEMHeader, readFIBSEMdat, lazyCachedCellImg
+from lib.io import loadFilePaths, readFIBSEMHeader, readFIBSEMdat, lazyCachedCellImg
 from lib.util import newFixedThreadPool
 from lib.ui import wrap
 from collections import defaultdict
@@ -10,18 +12,25 @@ from mpicbg.ij import SIFT # see https://github.com/axtimwalde/mpicbg/blob/maste
 from mpicbg.ij.clahe import FastFlat as CLAHE
 from java.util import ArrayList
 from java.util.concurrent import Callable
+from java.lang import Double
 from mpicbg.models import ErrorStatistic, TranslationModel2D, TransformMesh, PointMatch, NotEnoughDataPointsException, Tile, TileConfiguration
-from jarray import zeros
+from jarray import zeros, array
 from net.imglib2 import FinalInterval
 from net.imglib2.img.array import ArrayImgs
 from net.imglib2.algorithm.math import ImgMath
 from net.imglib2.type import PrimitiveType
+from net.imglib2.cache import CacheLoader
+from net.imglib2.type.numeric.integer import UnsignedShortType
+from net.imglib2.img.cell import Cell
+from ij.gui import Roi
+from ij.process import ShortProcessor
+from itertools import izip
 
 
 # Folders
 srcDir = "/home/albert/zstore1/FIBSEM/Pedro_parker/"
-tgtDir = "/home/albert/zstore1/FIBSEM/Pedro_parker/registration/"
-csvDir = "/home/albert/zstore1/FIBSEM/Pedro_parker/registration/csv/"
+tgtDir = "/home/albert/zstore1/FIBSEM/Pedro_parker/registration-Albert/"
+csvDir = "/home/albert/zstore1/FIBSEM/Pedro_parker/registration-Albert/csv/"
 
 # TODO ensure tgtDir and csvDir exist
 
@@ -40,10 +49,11 @@ paramsSIFT.maxOctaveSize = 0 # will be updated in a clone
 paramsSIFT.initialSigma = 1.6 # default 1.6
 paramsSIFT.fdSize = 8 # default is 4
 paramsSIFT.fdBins = 8 # default is 8
-
+    
 
 # Find all .dat files, as a sorted list
-filepaths = findFilePaths(srcDir, ".dat")
+filepaths = loadFilePaths(srcDir, ".dat", csvDir, "imagefilepaths")
+
 
 # Group files by section, as there could be multiple image tiles per section
 groups = defaultdict(list)
@@ -53,18 +63,18 @@ for filepath in filepaths:
   sectionName = filename[0:-9]
   groups[sectionName].append(filepath)
 
-for groupName, tilePaths in groups.iteritems():
-  if 1 == len(tilePaths) or 4 == len(titePaths):
+for groupName_, tilePaths_ in groups.iteritems():
+  if 1 == len(tilePaths_) or 4 == len(tilePaths_):
     pass
   else:
-    print "WARNING:", groupName, "has", len(tilePaths), "tiles"
+    print "WARNING:", groupName_, "has", len(tilePaths_), "tiles"
   
 
 class MontageSlice2x2(Callable):
-  def __init__(self, name, tilePaths, paramsSIFT, csvDir):
+  def __init__(self, groupName, tilePaths, paramsSIFT, csvDir):
     # EXPECTS 4 filepaths with filenames ending in ["_0-0-0.dat", "_0-0-1.dat", "_0-1-0.dat", "_0-1-1.dat"]
     # ASSUMES all tiles have the same dimensions
-    self.name = name
+    self.groupName = groupName
     self.tilePaths = list(sorted(tilePaths))
     self.paramsSIFT = paramsSIFT
     self.csvDir = csvDir
@@ -80,20 +90,20 @@ class MontageSlice2x2(Callable):
     
   def getFeatures(self, sp, roi):
     sp.setRoi(roi)
-    crop = sp.crop()
-    paramsSIFT = paramsSIFT.clone()
+    sp = sp.crop()
+    paramsSIFT = self.paramsSIFT.clone()
     paramsSIFT.minOctaveSize = min(sp.getWidth(), sp.getHeight())
     paramsSIFT.maxOctaveSize = max(sp.getWidth(), sp.getHeight())
     ijSIFT = SIFT(FloatArray2DSIFT(paramsSIFT))
     features = ArrayList() # of Feature instances
-    ijSIFT.extractFeatures(ip, features)
+    ijSIFT.extractFeatures(sp, features)
     return features
     
   def getPointMatches(self, sp0, roi0, sp1, roi1):
     features0 = self.getFeatures(sp0, roi0)
     features1 = self.getFeatures(sp1, roi1)
     model = TranslationModel2D()
-    pointMatches = FloatArray2DSIFT.createMatches(features0,
+    pointmatches = FloatArray2DSIFT.createMatches(features0,
                                                   features1,
                                                   self.params.get("max_sd", 1.5), # max_sd: maximal difference in size (ratio max/min)
                                                   model,
@@ -102,10 +112,12 @@ class MontageSlice2x2(Callable):
     return pointmatches
 
   def connectTiles(self, sps, tiles, i, j, roi0, roi1):
-    pointmatches = getPointMatches(sps[i], roi0, sps[j], roi1)
+    pointmatches = self.getPointMatches(sps[i], roi0, sps[j], roi1)
     tiles[i].connect(tiles[j], pointmatches) # reciprocal connection
     
   def loadShortProcessors(self):
+    for filepath in self.tilePaths:
+      print "#%s#" % filepath
     # Load images (TODO: should use FIBSEM_Reader instead)
     return [readFIBSEMdat(filepath, channel_index=0, asImagePlus=True)[0].getProcessor() for filepath in self.tilePaths]
 
@@ -114,7 +126,7 @@ class MontageSlice2x2(Callable):
     # extract features from the appropriate ROI along the overlapping edges
     
     # Check if matrices exist already:
-    matrices = loadMatrices(self.name, self.csvDir)
+    matrices = loadMatrices(self.groupName, self.csvDir)
     if matrices is not None:
       return matrices
     
@@ -134,12 +146,12 @@ class MontageSlice2x2(Callable):
     
     # Declare tile links
     tc = TileConfiguration()
-    tiles = [Tile(TranslationModel2D()) for _ in self.filePaths]
+    tiles = [Tile(TranslationModel2D()) for _ in self.tilePaths]
     tc.addTiles(tiles)
-    connectTiles(sps, tiles, 0, 1, roiLeft, roiRight)
-    connectTiles(sps, tiles, 2, 3, roiLeft, roiRight)
-    connectTiles(sps, tiles, 0, 2, roiTop, roiBottom)
-    connectTiles(sps, tiles, 1, 3, roiTop, roiBottom)
+    self.connectTiles(sps, tiles, 0, 1, roiLeft, roiRight)
+    self.connectTiles(sps, tiles, 2, 3, roiLeft, roiRight)
+    self.connectTiles(sps, tiles, 0, 2, roiTop, roiBottom)
+    self.connectTiles(sps, tiles, 1, 3, roiTop, roiBottom)
     tc.fixTile(tiles[0]) # top left tile
     
     # Optimise tile positions
@@ -155,21 +167,21 @@ class MontageSlice2x2(Callable):
       a = zeros(6, 'd')
       tile.getModel().toArray(a)
       matrices.append(array([a[0], a[2], a[4], a[1], a[3], a[5]], 'd'))
-    saveMatrices(self.name, matrices, self.csvDir)
+    saveMatrices(self.groupName, matrices, self.csvDir)
     return matrices
   
   def call(self):
-    return getMatrices()
+    return self.getMatrices()
 
   def montagedImg(self, width, height):
     """ Return an ArrayImg representing the montage
         width, height: dimensions of the canvas onto which to insert the tiles.
     """
-    matrices = getMatrices()
+    matrices = self.getMatrices()
     sps = self.loadShortProcessors()
-    impMontage = ShortProcessor(width, height, None)
+    impMontage = ShortProcessor(width, height)
     # Start pasting from the end, to bury the bad left edges
-    for sp, matrix in reverse(zip(sps, matrices)):
+    for sp, matrix in reversed(zip(sps, matrices)):
       impMontage.insert(sp, int(matrix[2] + 0.5), int(matrix[5] + 0.5)) # indices 2 and 5 are the X, Y translation
     
     return ArrayImgs.unsignedShorts(impMontage.getPixels(), width, height)
@@ -179,35 +191,42 @@ class SectionLoader(CacheLoader):
   """
   A CacheLoader where each cell is a section made from loading and transforming multiple tiles or just one tile
   """
-  def __init__(self, dimensions, groups, paramsSIFT, csvDir):
+  def __init__(self, dimensions, groupNames, tileGroups, paramsSIFT, csvDir):
     """
     """
     self.dimensions = dimensions # a list of [width, height] for the canvas onto which draw the image tiles
-    self.groups = groups # a list of lists of file paths to .dat files, one per section
+    self.groupNames = groupNames # list of names of each group, used to find its montage CSV if any
+    self.tileGroups = tileGroups # a list of lists of file paths to .dat files, one per section
     self.paramsSIFT = paramsSIFT
     self.csvDir = csvDir
   
   def get(self, index):
-    group = self.groups[index]
-    if 4 == len(group):
+    groupName = self.groupNames[index]
+    tilePaths = self.tileGroups[index]
+    if 4 == len(tilePaths):
       img = MontageSlice2x2(groupName, tilePaths, paramsSIFT, csvDir).montagedImg(*self.dimensions)
     elif 1 == len(group):
-      img = readFIBSEMdat(group[0], channel_index=0, asImagePlus=False)[0]
+      img = readFIBSEMdat(tilePaths[0], channel_index=0, asImagePlus=False)[0]
+    else:
+      # return empty Cell
+      print "WARNING: number of tiles isn't 4 or 1"
+      return Cell(self.dimensions + [1],
+                  [0, 0, index],
+                  ArrayImgs.unsignedShorts(self.dimensions).update(None)) # TODO this should be a constant DataAccess
       
-    if self.dimensions[0] == img.dimension[0] and self.dimensions[1] == img.dimension[1]:
+    if self.dimensions[0] == img.dimension(0) and self.dimensions[1] == img.dimension(1):
       aimg = img
     else:
       # copy it onto a new canvas
       aimg = ArrayImgs.unsignedShorts(self.dimensions)
       ImgMath.compute(ImgMath.img(img)).into(aimg)
   
-    dims = Intervals.dimensionsAsLongArray(aimg)
-    return Cell(list(dims) + [1], # cell dimensions
+    return Cell(self.dimensions + [1], # cell dimensions
                 [0] * aimg.numDimensions() + [index], # position in the grid: 0, 0, 0, Z-index
                 aimg.update(None)) # get the underlying DataAccess
 
 
-def ensureMontages2x2(groups, overlap, offset, paramsSIFT, csvDir):
+def ensureMontages2x2(groupNames, tileGroups, overlap, offset, paramsSIFT, csvDir):
   """
   Extract features and a matrix describing a TranslationModel2D for all tiles that need montaging.
   Each group must consist of 1 tile (no montage needed) or 4 tiles, which are assumed to be placed
@@ -216,9 +235,9 @@ def ensureMontages2x2(groups, overlap, offset, paramsSIFT, csvDir):
   The offset is for ignoring that many pixels from the left edge, which are artifactually
   non-linearly compressed and stretched in FIBSEM images. 
   
-  groups: a dictionary of lists, with the common part of the filename of all tiles in a section as the key,
-          and the value being the list of tile filenames.
-          In other words, each entry represents a section with 1 or 4 image tiles in it.
+  groupNames: a list of names, with the common part of the filename of all tiles in a section.
+  tileGroups: a list of lists of tile filenames.
+          In other words, these two lists are correlated, and each entry represents a section with 1 or 4 image tiles in it.
   overlap: the amount of pixels of overlap between two tiles in the 2x2 grid.
   offset: the amount of pixels to ignore from the left edge of an image tile.
   paramsSIFT: for montaging using scale invariant feature transform (SIFT).
@@ -230,14 +249,13 @@ def ensureMontages2x2(groups, overlap, offset, paramsSIFT, csvDir):
     futures = []
 
     # Iterate all sections in order and generate the transformation matrices defining a montage for each section
-    for groupName in sorted(groups.iterkeys()):
-      tilePaths = groups[groupName]
+    for groupName, tilePaths in izip(groupNames, tileGroups):
       # EXPECTING 2x2 tiles or 1
       if 4 == len(tilePaths):
         # Montage the tiles: compute a matrix detailing a TranslationModel2D for each tile
-        futures.append(MontageSlice2x2(groupName, tilePaths, paramsSIFT, csvDir))
+        futures.append(exe.submit(MontageSlice2x2(groupName, tilePaths, paramsSIFT, csvDir)))
       elif 1 == len(tilePaths):
-        futures.append(SingleTileSection(tilePaths[0]))
+        pass
       else:
         print "UNEXPECTED number of tiles in section named", groupName, ":", len(tilePaths)
 
@@ -248,18 +266,32 @@ def ensureMontages2x2(groups, overlap, offset, paramsSIFT, csvDir):
     exe.shutdown()
 
 
+# Sort groups by key
+keys = groups.keys()
+groupNames = []
+tileGroups = []
+for groupName in sorted(groups):
+  groupNames.append(groupName)
+  tileGroups.append(groups[groupName])
+
+
+# DEBUG: just one section
+groupNames = [groupNames[1000]]
+tileGroups = [tileGroups[1000]]
+
+
 # Montage all sections
-ensureMontages2x2(groups, overlap, offset, paramsSIFT, csvDir)
+ensureMontages2x2(groupNames, tileGroups, overlap, offset, paramsSIFT, csvDir)
 
 # Define a virtual CellImg expressing all the montages, one per section
 
 dimensions = [section_width, section_height]
-volume_dimensions = dimensions + [len(groups)]
+volume_dimensions = dimensions + [len(groupNames)]
 cell_dimensions = dimensions + [1]
 pixelType = UnsignedShortType
 primitiveType = PrimitiveType.SHORT
 
-volumeImg = lazyCachedCellImg(SectionLoader(dimensions, groups, paramsSIFT, csvDir),
+volumeImg = lazyCachedCellImg(SectionLoader(dimensions, groupNames, tileGroups, paramsSIFT, csvDir),
                               volume_dimensions,
                               cell_dimensions,
                               pixelType,
