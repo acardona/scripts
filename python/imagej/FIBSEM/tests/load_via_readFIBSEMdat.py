ad47ddcfd464c0f7879ee7efb831a39df38777a8
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2023-10-25 23:33:34 +0100

    Updated filepaths in test

--- python/imagej/FIBSEM/tests/load_via_readFIBSEMdat.py
+++ python/imagej/FIBSEM/tests/load_via_readFIBSEMdat.py
@@ -1,14 +1,26 @@
 import sys, os
-sys.path.append("/home/albert/lab/scripts/python/imagej/IsoView-GCaMP/")
+sys.path.append("/lmb/home/acardona/lab/scripts/python/imagej/IsoView-GCaMP/")
 
 from lib.io import readFIBSEMHeader, readFIBSEMdat
 
 #filepath = "/home/albert/zstore1/FIBSEM/Pedro_parker/M06/D15/Merlin-FIBdeSEMAna_23-06-15_000153_0-0-0.dat"
-filepath = "/home/albert/zstore1/FIBSEM/Pedro_parker/M06/D17/Merlin-FIBdeSEMAna_23-06-17_235001_0-0-0.dat"
+#filepath = "/home/albert/zstore1/FIBSEM/Pedro_parker/M06/D17/Merlin-FIBdeSEMAna_23-06-17_235001_0-0-0.dat"
 #filepath = "/home/albert/zstore1/FIBSEM/Pedro_parker/M06/D17/Merlin-FIBdeSEMAna_23-06-17_235001_0-0-1.dat"
 #rilepath = "/home/albert/zstore1/FIBSEM/Pedro_parker/M06/D17/Merlin-FIBdeSEMAna_23-06-17_235001_0-1-0.dat"
 #filepath = "/home/albert/zstore1/FIBSEM/Pedro_parker/M06/D17/Merlin-FIBdeSEMAna_23-06-17_235001_0-1-1.dat"
 
-imp = readFIBSEMdat(filepath, channel_index=0, asImagePlus=True)[0]
+#/net/zstore1/FIBSEM/Pedro_parker/M07/D10/Merlin-FIBdeSEMAna_23-07-10_170205_0-0-0.dat
+#/net/zstore1/FIBSEM/Pedro_parker/M07/D10/Merlin-FIBdeSEMAna_23-07-10_170205_0-0-1.dat
+#/net/zstore1/FIBSEM/Pedro_parker/M07/D10/Merlin-FIBdeSEMAna_23-07-10_170205_0-1-0.dat
+#/net/zstore1/FIBSEM/Pedro_parker/M07/D10/Merlin-FIBdeSEMAna_23-07-10_170205_0-1-1.dat
 
-imp.show()
+filepaths = [
+  "/net/zstore1/FIBSEM/Pedro_parker/M07/D10/Merlin-FIBdeSEMAna_23-07-10_170614_0-0-0.dat",
+  "/net/zstore1/FIBSEM/Pedro_parker/M07/D10/Merlin-FIBdeSEMAna_23-07-10_170614_0-0-1.dat",
+  "/net/zstore1/FIBSEM/Pedro_parker/M07/D10/Merlin-FIBdeSEMAna_23-07-10_170614_0-1-0.dat",
+  "/net/zstore1/FIBSEM/Pedro_parker/M07/D10/Merlin-FIBdeSEMAna_23-07-10_170614_0-1-1.dat"
+]
+
+for filepath in filepaths:
+  imp = readFIBSEMdat(filepath, channel_index=0, asImagePlus=True)[0]
+  imp.show()
