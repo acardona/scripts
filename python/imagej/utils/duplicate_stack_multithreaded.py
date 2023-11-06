f1f665cf8c55b62e1431b09457401c13b4f38084
Albert Cardona <sapristi@gmail.com>
2023-11-06 09:41:56 +0000

    Enable shallow copying.

--- python/imagej/utils/duplicate_stack_multithreaded.py
+++ python/imagej/utils/duplicate_stack_multithreaded.py
@@ -4,15 +4,17 @@ from java.lang import Runtime
 from ij import IJ, ImagePlus, ImageStack
 
 class Copy(Callable):
-  def __init__(self, ip):
+  def __init__(self, ip, shallow=False):
     self.ip = ip
+    self.shallow = shallow
   def call(self):
-    return self.ip.duplicate()
+    return self.ip if self.shallow else self.ip.duplicate()
 
-def duplicateInParallel(imp=None, slices=None, n_threads=0):
+def duplicateInParallel(imp=None, slices=None, n_threads=0, shallow=False):
   """ imp: defaults to None, meaning get the current image.
       slices: defaults to None, meaning all. Otherwise a list of 1-based indices.
       n_threads: defaults to 0, meaning as many as possible.
+      shallow: defaults to False, meaning don't share the pixel data.
   """
   imp = imp if imp else IJ.getImage()
   slices = slices if slices else range(1, imp.getNSlices() + 1)
@@ -20,7 +22,7 @@ def duplicateInParallel(imp=None, slices=None, n_threads=0):
   exe = Executors.newFixedThreadPool(n_threads if n_threads > 0 else min(Runtime.getRuntime().availableProcessors(), stack.getSize()))
   try:
     stack2 = ImageStack(imp.getWidth(), imp.getHeight())
-    futures = [(i, exe.submit(Copy(stack.getProcessor(i)))) for i in slices]
+    futures = [(i, exe.submit(Copy(stack.getProcessor(i), shallow))) for i in slices]
     for i, fu in futures:
       label = stack.getSliceLabel(i)
       stack2.addSlice(label if label else str(i), fu.get())
@@ -30,5 +32,5 @@ def duplicateInParallel(imp=None, slices=None, n_threads=0):
 
 
 imp = IJ.getImage()
-copy = duplicateInParallel(imp, range(3, 8), n_threads=10)
+copy = duplicateInParallel(imp, range(3, 8), n_threads=10, shallow=True)
 copy.show()
