f602e0f6b40c45ad3ae7d1c46eb75d80b0defb8e
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2023-11-07 11:38:51 +0000

    Update view_exported_N5.py to an example that works at the LMB.

--- python/imagej/FIBSEM/view_exported_N5.py
+++ python/imagej/FIBSEM/view_exported_N5.py
@@ -1,12 +1,12 @@
 import sys
-sys.path.append("/groups/cardona/home/cardonaa/lab/scripts/python/imagej/IsoView-GCaMP/")
+sys.path.append("/lmb/home/acardona/lab/scripts/python/imagej/IsoView-GCaMP/")
 from net.imglib2.view import Views
 from net.imglib2 import FinalInterval
 from lib.io import readN5
 from lib.ui import showStack
 
-name = "FIBSEM_L1116"
-img3D = readN5("/groups/cardona/cardonalab/FIBSEM_L1116_exports/n5/", name, show=None)
+name = "PedroParker"
+img3D = readN5("/net/zstore1/FIBSEM/Pedro_parker/registration-Albert/n5/", name, show=None)
 #fov = Views.interval(img3D, FinalInterval([4096, 4096, 0], [8192 -1, 8192 -1, 13770 -1]))
 fov = img3D # whole
 imp = showStack(fov, title=name)
