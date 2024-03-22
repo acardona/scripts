ebd1ad7b74fe437655c80324a78046063f3fd955
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2024-03-22 23:33:21 +0000

    minor

--- python/imagej/utils/duplicate_stack_multithreaded.py
+++ python/imagej/utils/duplicate_stack_multithreaded.py
@@ -12,7 +12,7 @@ class Copy(Callable):
     ip = self.stack.getProcessor(self.slice_index)
     return ip if self.shallow else ip.duplicate()
 
-def duplicateInParallel(imp=None, slices=None, n_threads=0, shallow=False):
+def duplicateInParallel(imp=None, slices=None, n_threads=0, shallow=False, show=True):
   """ imp: defaults to None, meaning get the current image.
       slices: defaults to None, meaning all. Otherwise a list of 1-based indices.
       n_threads: defaults to 0, meaning as many as possible.
@@ -28,7 +28,10 @@ def duplicateInParallel(imp=None, slices=None, n_threads=0, shallow=False):
     for i, fu in futures:
       label = stack.getSliceLabel(i)
       stack2.addSlice(label if label else str(i), fu.get())
-    return ImagePlus("%s - [%i, %i]" % (imp.getTitle(), slices[0], slices[-1]), stack2)
+    imp = ImagePlus("%s - [%i, %i]" % (imp.getTitle(), slices[0], slices[-1]), stack2)
+    if show:
+      imp.show()
+    return imp
   finally:
     exe.shutdown()
 
@@ -37,7 +40,11 @@ imp = IJ.getImage()
 # NC_Hypathia transition from 2x2 tiles to single tile
 #copy = duplicateInParallel(imp, range(3490, 3493), n_threads=10, shallow=True)
 
-copy = duplicateInParallel(imp, range(3470, 3520), n_threads=50, shallow=True)
-copy.show()
-#copy = duplicateInParallel(imp, range(1990, 2010), n_threads=50, shallow=True)
-#copy.show()
+#copy = duplicateInParallel(imp, range(3470, 3520), n_threads=50, shallow=True, show=True)
+
+#copy = duplicateInParallel(imp, range(1990, 2010), n_threads=50, shallow=True, show=True)
+
+
+#copy = duplicateInParallel(imp, range(9000, 9020), n_threads=50, shallow=True)
+
+copy = duplicateInParallel(imp, range(11885, 12054), n_threads=50, shallow=True, show=True)
