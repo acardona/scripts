4a6bd300e51a1449f0ddaa16109b74a2f210b2ef
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2024-03-27 15:22:48 +0000

    Fixed *again* the section offsets.

--- python/imagej/FIBSEM/serial_section_registration_NC_Hypathia8bit.py
+++ python/imagej/FIBSEM/serial_section_registration_NC_Hypathia8bit.py
@@ -89,15 +89,15 @@ def sectionOffsets(index): # index is 0-based
   # Must always return a tuple with two integers
   if index >= 10598:
     #return (5497, 496)
-    return (4748, 785)
+    return (4748, 2368)
   if index == 10597:
     #return (5478, 499)
-    return (4726, 787)
+    return (4726, 2370)
   if index >= 10595:
     #return (3471, 456)
-    return (3473, 455)   # (-752, +287) relative to below   
-  if index >= 8985:
-    return (4225, 167)
+    return (3473, 2038)   # (-752, +287) relative to below   
+  if index >= 8895:
+    return (4225, 1750)
   if index >= 3491: # index 3491 (0-based) is the first of the single-tile sections
     return (4225, 167)
   return (0, 0)
