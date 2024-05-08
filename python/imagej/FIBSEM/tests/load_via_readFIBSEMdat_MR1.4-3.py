5ffddca9139b6ec0e49f615d2d7853e54f72f4c8
Albert Cardona <sapristi@gmail.com>
2024-05-08 16:58:22 +0100

    More filepaths related to problematic sections of MR1.4-3 FIBSEM

--- python/imagej/FIBSEM/tests/load_via_readFIBSEMdat_MR1.4-3.py
+++ python/imagej/FIBSEM/tests/load_via_readFIBSEMdat_MR1.4-3.py
@@ -11,8 +11,16 @@ filepaths = [
   # 1x2:
   #"/net/fibserver1/raw/MR1.4-3/M03/D12/Merlin-WEMS_24-03-12_235936_0-0-0.dat",
   #"/net/fibserver1/raw/MR1.4-3/M03/D12/Merlin-WEMS_24-03-12_235936_0-1-0.dat"
-  "/net/fibserver1/raw/MR1.4-3/M03/D01/Merlin-WEMS_24-03-01_141202_0-0-0.dat",
-  "/net/fibserver1/raw/MR1.4-3/M03/D01/Merlin-WEMS_24-03-01_141202_0-1-0.dat"
+  #"/net/fibserver1/raw/MR1.4-3/M03/D01/Merlin-WEMS_24-03-01_141202_0-0-0.dat",
+  #"/net/fibserver1/raw/MR1.4-3/M03/D01/Merlin-WEMS_24-03-01_141202_0-1-0.dat",
+  #"/net/fibserver1/raw/MR1.4-3/M03/D05/Merlin-WEMS_24-03-05_124008_0-0-0.dat", # first of 4 montages that failed
+  #"/net/fibserver1/raw/MR1.4-3/M03/D05/Merlin-WEMS_24-03-05_124008_0-1-0.dat",
+  #"/net/fibserver1/raw/MR1.4-3/M03/D05/Merlin-WEMS_24-03-05_163336_0-0-0.dat", # second of 4 montages that failed
+  #"/net/fibserver1/raw/MR1.4-3/M03/D05/Merlin-WEMS_24-03-05_163336_0-1-0.dat",
+  #"/net/fibserver1/raw/MR1.4-3/M03/D05/Merlin-WEMS_24-03-05_125417_0-0-0.dat", # third of 4 montages that failed
+  #"/net/fibserver1/raw/MR1.4-3/M03/D05/Merlin-WEMS_24-03-05_125417_0-1-0.dat",
+  "/net/fibserver1/raw/MR1.4-3/M03/D05/Merlin-WEMS_24-03-05_134004_0-0-0.dat", # fourth of 4 montages that failed
+  "/net/fibserver1/raw/MR1.4-3/M03/D05/Merlin-WEMS_24-03-05_134004_0-1-0.dat"
 ]
 
 
