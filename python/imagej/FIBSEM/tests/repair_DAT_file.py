000e0a3a3ec2543613734bc0ecb136281fffe88e
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2024-05-16 14:05:27 +0000

    Repair MR1.4-3 DAT files

--- python/imagej/FIBSEM/tests/repair_DAT_file.py
+++ python/imagej/FIBSEM/tests/repair_DAT_file.py
@@ -2,63 +2,57 @@
 # Rescues as much data as there may be from the first channel
 # into a new file.
 
-import os
+import os, sys
+sys.path.append("/lmb/home/acardona/lab/scripts/python/imagej/IsoView-GCaMP/")
 from jarray import zeros
 from java.io import RandomAccessFile, File
 from java.lang import System
+from lib.io import repairTruncatedDAT, readFIBSEMdat, blankROIDAT
 
-basepath = "/net/fibserver1/raw/MR1.4-3/M02/D27/"
-good = "Merlin-WEMS_24-02-27_165658_0-0-0.dat"
-bad  = "Merlin-WEMS_24-02-27_165658_0-1-0.dat"
+sourcePath = "/net/fibserver1/raw/MR1.4-3/"
+repairedPath = "/net/zstore1/FIBSEM/MR1.4-3/repaired/"
 
-repaired = "/net/zstore1/FIBSEM/MR1.4-3/repaired/" + bad
+# List of images to repair with a reference image to use for correcting header and dimensions
+pairs = [
+  {"good": "M02/D27/Merlin-WEMS_24-02-27_165658_0-0-0.dat",
+   "bad" : "M02/D27/Merlin-WEMS_24-02-27_165658_0-1-0.dat"}, # truncated
+  {"good": "M02/D27/Merlin-WEMS_24-02-27_201135_0-0-0.dat",
+   "bad" : "M02/D27/Merlin-WEMS_24-02-27_201135_0-1-0.dat"}, # truncated
+  {"good": "M03/D05/Merlin-WEMS_24-03-05_062018_0-1-0.dat",
+   "bad" : "M03/D05/Merlin-WEMS_24-03-05_062018_0-0-0.dat"}, # truncated: but when montaging should end up at the bottom
+  {"good": "M03/D10/Merlin-WEMS_24-03-10_054103_0-0-0.dat",
+   "bad" : "M03/D10/Merlin-WEMS_24-03-10_054103_0-1-0.dat"}, # truncated
+  {"good": "M03/D13/Merlin-WEMS_24-03-13_235528_0-0-0.dat",
+   "bad" : "M03/D13/Merlin-WEMS_24-03-13_235528_0-1-0.dat"}, # truncated
+  {"good": "M03/D15/Merlin-WEMS_24-03-15_130137_0-0-0.dat",
+   "bad" : "M03/D15/Merlin-WEMS_24-03-15_130137_0-1-0.dat"}, # truncated
+  {"good": "M02/D23/Merlin-WEMS_24-02-23_213431_0-0-0.dat",
+   "bad" : "M02/D23/Merlin-WEMS_24-02-23_213519_0-0-0.dat"}, # truncated
+]
 
-
-def repair(goodFilePath, badFilePath, repairedFilePath, header=1024):
-  # ASSUMES 2 channels
-  # Copy header and trailer from the good file
-  raGood = RandomAccessFile(goodFilePath, 'r')
-  try:
-    # Parse width and height
-    raGood.seek(100)
-    width = raGood.readInt()
-    raGood.seek(104)
-    height = raGood.readInt()
-    # Copy header
-    headerBytes = zeros(header, 'b')
-    raGood.seek(0)
-    raGood.read(headerBytes)
-    # Copy trailer
-    goodLength = File(goodFilePath).length()
-    start_of_trailer = 1024 + width * height * 4
-    raGood.seek(1024 + width * height * 4)
-    trailerBytes = zeros(goodLength - start_of_trailer, 'b')
-    raGood.read(trailerBytes)
-  finally:
-    raGood.close()
-  
-  # From the bad file, copy as many pixels as there are.
-  # The assumption is that the file writing was truncated, so the header exists (but may be corrupted)
-  # and the pixels array is truncated, and the trailer is not there.
-  raBad = RandomAccessFile(badFilePath, 'r')
-  raRepaired = RandomAccessFile(repairedFilePath, 'rw')
-  try:
-    repairedBytes = zeros(header + width * height * 4 + len(trailerBytes), 'b') # assumes 2 channels of 16-bit
-    System.arraycopy(headerBytes, 0, repairedBytes, 0, header)
-    badLength = File(badFilePath).length()
-    raBad.seek(header)
-    raBad.read(repairedBytes, header, badLength - header)
-    System.arraycopy(trailerBytes, 0, repairedBytes, goodLength - len(trailerBytes), len(trailerBytes)) 
-    raRepaired.write(repairedBytes)
-    raRepaired.getFD().sync()
-  finally:
-    raBad.close()
-    raRepaired.close()
+# List of images with ROIs to blank out
+to_blank = [
+  #{"path": "M02/D23/Merlin-WEMS_24-02-23_213519_0-0-0.dat",  # turns out it's truncated by opens funny, with a duplication
+  # "rois": [[0, 9815, 12500, 2685]]}, # [x,y,width,height]
+]
 
 
-repair(os.path.join(basepath, good),
-       os.path.join(basepath, bad),
-       repaired)
+for p in pairs:
+  good = p["good"]
+  bad = p["bad"]
+  repairTruncatedDAT(os.path.join(sourcePath, good),
+                     os.path.join(sourcePath, bad),
+                     os.path.join(repairedPath, bad[bad.rfind('/')+1:]))
 
-  
-  
+
+for p in to_blank:
+  blankROIDAT(os.path.join(sourcePath, p["path"]),
+              os.path.join(repairedPath, p["path"][p["path"].rfind('/')+1:]),
+              p["rois"])
+
+
+# Open all repaired files
+for relpath in [p["bad"] for p in pairs] + [p["path"] for p in to_blank]:
+  path = os.path.join(repairedPath, relpath[relpath.rfind('/')+1:])
+  imp = readFIBSEMdat(path, channel_index=0, asImagePlus=True)[0]
+  imp.show()
