40c73706ca1c8d143da46741fc2a887844329357
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2024-06-19 22:48:22 +0000

    concat_imgs.py for MR1.4-3: splice at 16400 (1-based) And don't wrap but IL.wrap to avoid adding a channels dimension. And properly view the translated interval of the second volume.

--- python/imagej/FIBSEM/MR1.4-3/concat_imgs.py
+++ python/imagej/FIBSEM/MR1.4-3/concat_imgs.py
@@ -6,22 +6,22 @@
 import sys, os
 sys.path.append("/lmb/home/acardona/lab/scripts/python/imagej/IsoView-GCaMP/")
 from net.imglib2.view import Views
-from net.imglib2.img.display.imagej import ImageJVirtualStack
+from net.imglib2.img.display.imagej import ImageJVirtualStack, ImageJFunctions as IL
 from ij import WindowManager
 from lib.ui import wrap
 
 # First image: from 0 to lastIndexZ1 (0-based)
-lastIndexZ1 = 16999
+lastIndexZ1 = 16399
 # Second image: from firstIndexZ2 to the last section (0-based)
-firstIndexZ2 = 17000
+firstIndexZ2 = 16400
 
 # Text tags on the title of the open images
 titleSuffix1 = "bad" # the old one, to keep from 0 to 16999
 titleSuffix2 = "good" # the new one, to appeld from 17000 to the end
 
 # Translation in 2D of the second image
-dx2 = 279 # 278.5
-dy2 = 67 # 66.6
+dx2 = -1 # -0.975
+dy2 = 0 # -0.179
 
 
 def run():
@@ -54,18 +54,24 @@ def run():
   img2 = f.get(imp2.getStack())
 
   maxCoords2D = [img1.dimension(i) -1 for i in xrange(2)]
+  
+  vd = Views.interval(Views.extendZero(img2),
+                      [0, 0, firstIndexZ2],
+                      maxCoords2D + [img2.dimension(2) -1])
 
   merged = Views.concatenate(2, # Z axis
                              Views.interval(img1,
                                             [0, 0, 0],
                                             maxCoords2D + [lastIndexZ1]),
-                             Views.translate(
-                               Views.interval(Views.extendZero(img2),
-                                              [0, 0, firstIndexZ2],
-                                              maxCoords2D + [img2.dimension(2) -1]),
-                               [dx2, dy2, 0]))
+                             Views.zeroMin(
+                               Views.interval(
+                                 Views.translate(vd, [dx2, dy2, 0]),
+                                 vd)))
+                                 
 
-  imp3 = wrap(merged)
+  imp3 = IL.wrap(merged, "MR1.4-3")
+  if imp1.getRoi():
+    imp3.setRoi(imp1.getRoi())
   imp3.show()
 
 
