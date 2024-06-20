dff42610176303c237cdd4ccee557348f76a275f
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2024-06-20 20:28:51 +0000

    Fix error

--- python/imagej/FIBSEM/MR1.4-3/update_N5.py
+++ python/imagej/FIBSEM/MR1.4-3/update_N5.py
@@ -23,7 +23,7 @@ missing = Views.interval(imgOld,
                           imgOld.dimension(1) -1,
                           16399])
              
-n5writeNew = N5Factory().openWriter(os.path.join(n5pathNew, n5pathNew))
+n5writeNew = N5Factory().openWriter(os.path.join(n5pathNew, n5nameNew))
 N5Utils.saveRegion(missing, n5writeNew, "/") # path is relative
 
 impNew.setNSlice(16400)
