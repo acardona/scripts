baba0ee63a6306f919bed2ffa607f3d0aa91afa6
Albert Cardona <sapristi@gmail.com>
2024-06-26 12:38:51 +0100

    Updated asmifier.sh and compile.sh to latest asm, imglib2, and mpicbg libraries

--- java/asm/asmifier.sh
+++ java/asm/asmifier.sh
@@ -1,2 +1,2 @@
 # $1 is a .class file like #my/UnsignedByteToFloatSamplerConverter.class
-/usr/lib/jvm/java-8-openjdk-amd64/bin/java -classpath /home/albert/Programming/fiji-new/Fiji.app/jars/imglib2-5.12.0.jar:/home/albert/Programming/fiji-new/Fiji.app/jars/asm-7.1.jar://home/albert/Programming/fiji-new/Fiji.app/jars/asm-util-7.1.jar org.objectweb.asm.util.ASMifier $1
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -classpath /home/albert/Programming/fiji-new/Fiji.app/jars/imglib2-6.1.0.jar:/home/albert/Programming/fiji-new/Fiji.app/jars/asm-9.6.jar:/home/albert/Programming/fiji-new/Fiji.app/jars/asm-util-9.6.jar org.objectweb.asm.util.ASMifier $1
