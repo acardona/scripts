bdb07df08f4ec326cfd6b6871511d0724aea7fbf
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2025-03-16 21:29:57 +0000

    lib/ui.py: function grabImg

--- python/imagej/IsoView-GCaMP/lib/ui.py
+++ python/imagej/IsoView-GCaMP/lib/ui.py
@@ -5,6 +5,7 @@ from lib.converter import createConverter, convert
 from net.imglib2 import FinalInterval
 from net.imglib2.img.array import ArrayImgs
 from net.imglib2.img.display.imagej import ImageJFunctions as IL
+from net.imglib2.img.display.imagej import ImageJVirtualStack
 from net.imglib2.type.numeric.real import FloatType
 from net.imglib2.util import ImgUtil
 from net.imglib2.view import Views
@@ -38,7 +39,20 @@ def wrap(img, title="", n_channels=1):
       d -= 1
   #
   return IL.wrap(img, title)
-  
+
+
+def grabImg(imp):
+  """ Return the ImgLib2 image wrapped by the ImagePlus imp. """
+  stack = imp.getStack() # an ImageJVirtualStackUnsignedShort or similar (for each pixel type) which extends ImageJVirtualStack which has a source field with the ImgLib2 RandomAccessibleInterval instance
+  print type(stack)
+
+  if isinstance(stack, ImageJVirtualStack):
+    # Make the private field accessible
+    f = ImageJVirtualStack.getDeclaredField("source")
+    f.setAccessible(True)
+    img = f.get(stack)
+    return img
+
 
 def showAsStack(images, title=None, show=True):
   if not title:
