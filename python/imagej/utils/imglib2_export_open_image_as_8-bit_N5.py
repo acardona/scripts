0c78f1d7e91050262681ff1ca95f23b2ab91af0b
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2025-03-16 21:32:54 +0000

    export for MR1.4-3 n5-2b

--- python/imagej/utils/imglib2_export_open_image_as_8-bit_N5.py
+++ python/imagej/utils/imglib2_export_open_image_as_8-bit_N5.py
@@ -68,8 +68,14 @@ def exportOpenVSAs8bitN5(name, # dataset name
 #                     gzip_compression=4,
 #                     n_threads=128)
  
-exportOpenVSAs8bitN5("MR1.4-2",
-                     "/net/zstore1/FIBSEM/MR1.4-2/registration/n5/",
+#exportOpenVSAs8bitN5("MR1.4-2",
+#                     "/net/zstore1/FIBSEM/MR1.4-2/registration/n5/",
+#                     [256, 256, 64],
+#                     gzip_compression=4,
+#                     n_threads=128)
+
+exportOpenVSAs8bitN5("MR1.4-3",
+                     "/net/zstore1/FIBSEM/MR1.4-3/registration/n5-2b/",
                      [256, 256, 64],
                      gzip_compression=4,
                      n_threads=128)
