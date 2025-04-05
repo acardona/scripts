14b927cb08acc3afaeeaeadc60255be47c47f20a
Albert Cardona <sapristi@gmail.com>
2025-04-05 01:51:21 +0100

    Steps 5 and 6 for FIBSEM registration: blockmatching alignment.

---
+++ python/imagej/FIBSEM/registration/step_6_blockmatching_registration.py
@@ -0,0 +1,47 @@
+# Step 6: run the blockmatching-based registration in chunks
+
+import sys, os
+# Add current directory to path
+sys.path.append(os.path.dirname(sys.argv[0]))
+
+# Import parameters
+from step_1_montage_parameters import libDir, \
+             name, srcDir, tgtDir, montageDir, repairedDir, \
+             offset, overlap, nominal_overlap, \
+             section_width, section_height, \
+             first_section, last_section, replace_sections, \
+             params_pixels, paramsSIFT, paramsRANSAC, paramsTileConf, \
+             to_remove, ignore_images, replace_images
+
+from step_3_SIFT_registration_parameters import SIFTdir, properties, \
+             paramsSIFT, paramsPMs, paramsTileConfiguration
+             
+from step_5_blockmatching_registration_parameters import BMdir, propertiesBM, paramsBlockMatching, paramsTileConfigurationBM
+
+# Import registration library functions
+sys.path.append(libDir)
+from lib.montage2d import runMontaging
+from lib.serial2Dregistration import runSIFTAlignment
+from lib.serial2Dregistration import runBlockMatchingAlignment
+
+
+# Open the stack of scaled montages
+volumeImgMontaged, groupNames, tileGroups = runMontaging(
+             name, srcDir, tgtDir, montageDir, repairedDir,
+             offset, overlap, nominal_overlap,
+             section_width, section_height,
+             first_section, last_section, replace_sections,
+             params_pixels, paramsSIFT, paramsRANSAC, paramsTileConf,
+             to_remove, ignore_images, replace_images,
+             showTable=False, show=False)
+
+# Open the stack pre-aligned with SIFT features using chunks
+imgSIFT, impSIFT, matricesSIFT = runSIFTAlignment(
+             volumeImgMontaged, groupNames, SIFTdir,
+             properties, paramsSIFT, paramsPMs, paramsTileConfiguration, params_pixels)
+
+# Open the blockmatching finely aligned using chunks
+imgBM, impBM, matricesBM = runBlockMatchingAlignment(
+             imgSIFT, matricesSIFT, volumeImgMontaged,
+             groupNames, BMdir,
+             propertiesBM, paramsBlockMatching, paramsTileConfigurationBM)
