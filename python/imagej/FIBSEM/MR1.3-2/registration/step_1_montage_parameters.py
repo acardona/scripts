d512b871132d5bb8a107b7029961f081953b63a5
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2025-04-08 16:42:41 +0000

    FIBSEM registratiion for MR1.3-2

---
+++ python/imagej/FIBSEM/MR1.3-2/registration/step_1_montage_parameters.py
@@ -0,0 +1,105 @@
+# Step 1: montage parameters
+
+from mpicbg.imagefeatures import FloatArray2DSIFT
+from ij.gui import Roi
+
+# REGISTRATION LIBRARY
+libDir = "/lmb/home/acardona/lab/scripts/python/imagej/IsoView-GCaMP/"
+
+# VOLUME
+name = "MR1.3-2" # Name of the folder containing the .dat files, e.g., "MR1.4-3"
+sourceServer = "/net/fibserver1/raw/"
+targetServer = "/net/zstore1/FIBSEM/"
+
+
+# Folders
+srcDir = sourceServer + name + "/"
+tgtDir = targetServer + name + "/registration/"
+montageDir = tgtDir + "montage-csv/" # for in-section montaging
+repairedDir = targetServer + name + "/repaired/" # Folder with repaired images, if any
+
+# Image tile overlap parameters
+offset  =  80 # pixels The left margin of each image is severely elastically deformed.
+overlap = 990 # pixels
+nominal_overlap = 1000 # 8 microns at 8 nm/px = 1000 px
+
+# Working canvas
+section_width  = 16000  # should have used 14400    # pixels, after section-wise montaging
+section_height = 16000  # should have used 12800   # So a canvas of 256,000,000 pixels: 256 MB
+
+# Skip sections. Define a range to work with.
+first_section = 2868  # 0-based
+last_section = 20964
+
+# Replace sections: (0-based, not 1-based !)
+# NOTE indices are relative to the first_section as specified above
+# Add entries like: 1718: 1719,  indicating that section 1718 is to be replaced by section at 1719, effectively duplicating the latter
+# This is desirable to keep the true Euclidean distances and dimensions while overriding a faulty section with e.g, truncated images.
+replace_sections = {
+  1750: 1749, # 1750 is missing data at the bottom
+  1751: 1752, # 1751 is missing data
+  7108: 7107, # 7108 is missing data at the top, the top tile had the wrong magnification
+  8136: 8135, # 8136 is missing the top tile
+}
+
+# Image contrast parameters
+# The roiFn is for using only e.g., the central part of the canvas to compute the display range.
+# The contrast is for limiting the min and max to histogram bins that reach that many pixel counts,
+# starting from each end respectively.
+params_pixels = {
+  "invert": True,
+  "CLAHE_params": [200, 255, 2.0], # blockRadius, n_bins, and slope in stdDevs
+  "as8bit": True,
+  "contrast": (500, 1000), # thresholds in pixel counts per histogram bin
+  "roiFn": lambda sp: Roi(sp.width / 6, sp.height / 6, 2 * sp.width / 3, 2 * sp.height / 3), # middle 2/3rds to discard edges
+  "interim_scale": 0.25, # for saving montaged snapshops to disk to be used for evaluation and serial alignment
+}
+
+# Parameters for SIFT features
+paramsSIFT = FloatArray2DSIFT.Param()
+paramsSIFT.steps = 1
+paramsSIFT.minOctaveSize = 0 # will be updated in a clone
+paramsSIFT.maxOctaveSize = 0 # will be updated in a clone
+paramsSIFT.initialSigma = 1.6 # default 1.6
+paramsSIFT.fdSize = 8 # default is 4
+paramsSIFT.fdBins = 8 # default is 8
+
+paramsRANSAC = {
+  "iterations": 1000,
+  "maxEpsilon": 10, # pixels, maximum error allowed, usual number is 25.
+  "minInlierRatio": 0.01 # 1%
+}
+
+# For intra-section montages:
+paramsTileConf = {
+  "maxAllowedError": 0, # Saalfeld recommends 0
+  "maxPlateauwidth": 200, # Like in TrakEM2
+  "maxIterations": 1000, # Saalfeld recommends at least 1000
+  "damp": 1.0, # Saalfeld recommends 1.0, which means no damp
+  "nThreadsOptimizer": 10 # for the TileUtil.optimizeConcurrently for each montage.
+                          # Consider that (numCPUs / (nThreadsOptimizer/2)) sections will be montaged concurrently.
+}
+
+# Sections known to have problems (found via check = True above)
+# Add entries like this, with a comment for the record":
+# "Merlin-WEMS_24-02-27_170732_" # added 0-0-0 tile to ignore: truncated, no pixels, only header
+to_remove = set([
+  
+])
+
+# Add entries like this, with the file name of individual image tiles and a comment:
+# "Merlin-WEMS_24-02-27_170732_0-0-0.dat", # only header, whole image truncated
+ignore_images = set([
+  "Merlin-WEMS_24-02-27_170732_0-0-0.dat", # only header, whole image truncated
+  "Merlin-WEMS_24-03-05_062018_0-0-0.dat"  # partial truncation without sample in it, would occlude the 0-1-0 tile
+])
+
+# Replacement files to be found under the repaired folder:
+# Add key:value entries like this:
+# "Merlin-WEMS_24-03-02_190309_0-0-0.dat": "Merlin-WEMS_24-03-02_190309_0-0-0.tif",
+replace_images = {
+  "Merlin-WEMS_24-03-02_190309_0-0-0.dat": "Merlin-WEMS_24-03-02_190309_0-0-0.tif",  
+}
+
+
+
---
+++ python/imagej/FIBSEM/MR1.3-2/registration/step_3_SIFT_registration_parameters.py
@@ -0,0 +1,77 @@
+# Step 3: SIFT registration parameters
+
+import sys, os
+from mpicbg.imagefeatures import FloatArray2DSIFT
+from ij.gui import Roi
+# Add current directory to path
+sys.path.append(os.path.dirname(sys.argv[0]))
+# Import parameters used for montaging
+from step_1_montage_parameters import libDir, name, tgtDir, montageDir, section_width, section_height, params_pixels
+# Import registration library functions
+sys.path.append(libDir)
+from lib.serial2Dregistration import handleNoPointMatches, makeFilterFeaturesFn
+from lib.util import numCPUs
+from java.lang import Double
+
+
+# Folder for storing SIFT features per montage and pointmatches across montages, and the matrices.csv file
+SIFTdir = tgtDir + "SIFT-csv/"
+
+
+# EDIT below until the end if needed
+
+# Parameters to filter out features outside the tissue using a LabKit model
+# Can be None
+model_path = None # os.path.join("/net/zstore1/FIBSEM/MR1.4-3/registration/MR1.4-3_section1+6000_0.025.labkit.classifier") # from LabKit
+model_width = None # 400 # target width for resizing so as to match the dimensions of the image used when training the model.
+
+
+# Parameters for extracting SIFT features
+properties = {
+ 'name': name,
+ 'scale': 0.5, # Compounds with montaging interim_scale
+ 'n_threads': numCPUs(),
+ 'roi': [3600, # x      -  [x, y, width, height] or None. To e.g. extract SIFT features from center part only.
+         1800, # y     NOTE: in full image resolution coordinates.
+         7200, # width
+         8200], # height
+ 'RANSAC_iterations': 1000,
+ 'RANSAC_maxEpsilon': 25, # default is 25, for ssTEM 40nm sections cross-section alignment, but FIBSEM at 8nm sections is far thinner
+ 'RANSAC_minInlierRatio': 0.01,
+ 'handleNoPointMatchesFn': handleNoPointMatches, # Amounts to no translation, with a single PointMatch at 0,0
+ 'filterFeaturesFn': None, # makeFilterFeaturesFn(model_path, model_width), # Filter out features not in the tissue but in the resin, to ignore the resin which has streaks and curtains
+}
+
+
+# Parameters for SIFT features
+paramsSIFT = FloatArray2DSIFT.Param()
+paramsSIFT.fdSize = 8 # default is 4
+paramsSIFT.fdBins = 8 # default is 8
+paramsSIFT.maxOctaveSize = 500 # effective scale: section_width=16000, interim_scale=0.25 (4000), so 0.25/8 !
+paramsSIFT.steps = 3
+paramsSIFT.minOctaveSize = 250
+paramsSIFT.initialSigma = 1.6 # default 1.6
+
+# Parameters for pointmatches
+paramsPMs = {
+  'scale': properties.get('scale', 0.5), # compounds with montage interim_scale
+  'rod': 0.9, # ratio of best vs second best
+  'max_sd': 1.5, # maximal difference in size (ratio max/min)
+  'max_id': Double.MAX_VALUE, # max allowed distance between features in full image space
+}
+
+# Parameters for computing the SIFT-based transformation models
+paramsTileConfiguration = {
+  "n_adjacent": 3, # minimum of 1; Number of adjacent sections to pair up
+  "maxAllowedError": 0, # Saalfeld recommends 0
+  "maxPlateauwidth": 200, # Like in TrakEM2
+  "maxIterations": 20000, # Optimizer iterations for each chunk of chunk_size sections
+                         # Using a low number like 1000 to start with. If one chunk fails
+                         # to align well, re-run that chunk with more iterations (like 20x, 20000).
+  "damp": 1.0, # Saalfeld recommends 1.0, which means no damp
+  "nThreadsOptimizer": numCPUs(), # as many as CPU cores
+  "chunk_size": 400, # Will align in 50% overlapping chunks for best use of the optimizer
+  "chunk_maxIterations": 100000, # Iterations for the cross-chunk alignment
+  "fixed_tile_index": 2050, # None implies use the middle tile. Otherwise provide an index (0-based)
+}
+
