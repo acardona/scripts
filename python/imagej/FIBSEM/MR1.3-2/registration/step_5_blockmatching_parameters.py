1777efdeaa7c84f09bc9d576975eff8229f842b3
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2025-04-17 09:01:33 +0000

    FIBSEM MR1.3-2: update parameters for blockmatching.
    Otherwise takes too long.

--- python/imagej/FIBSEM/MR1.3-2/registration/step_5_blockmatching_parameters.py
+++ python/imagej/FIBSEM/MR1.3-2/registration/step_5_blockmatching_parameters.py
@@ -15,17 +15,18 @@ from lib.util import numCPUs
 
 
 # Folder for storing blockmatching features per montage and pointmatches across montages, and the matrices.csv files
-BMdir = tgtDir + "BM-csv/"
+#BMdir = tgtDir + "BM-csv/"
+BMdir = "/data1/acardona/MR1.3-2/registration/BM-csv/" # local to cardona-cpu1 server
 
 
 propertiesBM = {
  'name': name,
  'scale': 0.5, # Compounds with montaging interim_scale
  'n_threads': numCPUs(),
- 'roi': None, #[2000,  # To extract SIFT features from e.g., center part only, reducing ops by 4x
-         #2400, # [x, y, width, height] or None.
-         #8000,
-         #10000],
+ 'roi': [2000,  # To extract SIFT features from e.g., center part only, reducing ops by 4x
+         2400, # [x, y, width, height] or None.
+         8000,
+         10000],
  'handleNoPointMatchesFn': handleNoPointMatches, # Amounts to no translation, with a single PointMatch at 0,0
  'filterFeaturesFn': None, #makeFilterFeaturesFn(model_path, model_width), # Filter out features not in the tissue but in the resin, to ignore the resin which has streaks and curtains
 }
@@ -33,11 +34,11 @@ propertiesBM = {
 # Parameters for blockmatching
 paramsBlockMatching = {
  'scale': propertiesBM['scale'], # Compounds with montaging interim_scale, so 0.5 would mean half of that
- 'meshResolution': 20, # 10x10 = 100 points
+ 'meshResolution': 10, # 10x10 = 100 points
  'minR': 0.1, # min PMCC (Pearson product-moment correlation coefficient)
  'rod': 0.9, # max second best r / best r
  'maxCurvature': 1000.0, # default is 10
- 'searchRadius': 200, # Maximum expected displacement between slices after SIFT-based registration.
+ 'searchRadius': 100, # Maximum expected displacement between slices after SIFT-based registration.
                       # Make it large enough, 300 is a good first searcRadius value. 50 to a 100 for a fast run.
  'blockRadius': 200, # small, yet enough: size of the window to use for comparing across images.
 }
