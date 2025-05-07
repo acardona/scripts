f3822c5a7cf47910f570b6cdb13d2ce53bd79196
Albert Cardona <sapristi@gmail.com>
2025-05-07 15:59:11 +0100

    New methods to ImgCompare for cosyne similarity normalizing the 2D images before computing the dot product.

--- java/asm/my/ImgCompare.java
+++ java/asm/my/ImgCompare.java
@@ -33,4 +33,34 @@ public class ImgCompare {
     
     return sumMul / ( Math.sqrt(sumSq1) + Math.sqrt(sumSq2) );
   }
+
+	/** The sqrtSumSq parameters are used to normalize the pixel value prior to computing the dot product. */
+  static public double cosyneSimilarityNorm(final IterableInterval<RealType<?>> img1,
+			                                      final double sqrtSumSq1,
+                                            final IterableInterval<RealType<?>> img2,
+																				    final double sqrtSumSq2) {
+    final Cursor<RealType<?>> c1 = img1.cursor(),
+                              c2 = img2.cursor();
+    
+    double sumMul = 0,
+           sumSq1 = 0,
+           sumSq2 = 0;
+    
+    while (c1.hasNext()) {
+    	final double v1 = c1.next().getRealDouble() / sqrtSumSq1,
+    	             v2 = c2.next().getRealDouble() / sqrtSumSq2;
+    	sumMul += v1 * v2;
+    }
+    
+    return sumMul;
+  }
+
+	static public double sqrtSumSquares(final IterableInterval<RealType<?>> img) {
+		final Cursor<RealType<?>> c = img.cursor();
+		double sum = 0;
+		while (c.hasNext()) {
+			sum += Math.pow(c.next().getRealDouble(), 2);
+		}
+		return Math.sqrt(sum);
+	}
 }
