5d357594cca4f543881fe0f26866b6141d6af15c
Albert Cardona <sapristi@gmail.com>
2025-10-01 13:57:21 +0100

    montage2d_table.py: Fix sorting of inlier stats: was strings!

--- python/imagej/IsoView-GCaMP/lib/montage2d_table.py
+++ python/imagej/IsoView-GCaMP/lib/montage2d_table.py
@@ -467,7 +467,7 @@ def makeMontageTable(groupNames, tileGroups, imp, volumeImg, csvDir, show=True):
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         reader.next() # skip the header
         inlier_counts = [n_inliers for _, _, n_inliers in reader] # all as strings
-        montage_stats.append([min(inlier_counts), ", ".join(inlier_counts)])
+        montage_stats.append([min(int(s) for s in inlier_counts), ", ".join(inlier_counts)])
     else:
       # Either it was deleted or was never written, from a prior version of this software
       montage_stats.append([0, "no stats file"])
