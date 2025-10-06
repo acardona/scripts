938503670b6d32acafb39e69aa13c04f456d787e
Peter Hague <pete.hague@gmail.com>
2025-10-06 11:21:55 +0100

    Added script to update montaging folder from trackem output

--- python/imagej/FIBSEM/NGC-10/get_trackem.py
+++ python/imagej/FIBSEM/NGC-10/get_trackem.py
@@ -1,6 +1,29 @@
-
 from ini.trakem2.display import Display
+import os
 
+print "\n"
+start=True
+outputstring = '"m00","m01","m02","m10","m11","m12"'
 for patch in Display.getFront().getLayer().getPatches(True):
-  print patch
-  print patch.getAffineTransform()
+  if start:
+  	fileroot = "_".join(patch.filePath.split("_")[0:3]) + "_"
+  	fileroot = fileroot.replace("tmp/", "")
+  	print fileroot+".csv"
+  	roottrans = patch.getAffineTransform()
+  	rootx = roottrans.getTranslateX()
+  	rooty = roottrans.getTranslateY()
+  	print "Root tile:",rootx, rooty
+  	start = False
+  transform = patch.getAffineTransform()
+  tx = transform.getTranslateX()
+  ty = transform.getTranslateY()
+  outputstring += "\n1.0,0.0,{0},0.0,1.0,{1}".format(tx-rootx, ty-rooty)
+print(outputstring)
+
+with open(fileroot+".csv", "w") as f:
+	f.write(outputstring)
+	
+imfile = fileroot.replace("montage-csv", "montage-csv/scaled-montages")+".tif"
+os.remove(imfile)
+print "Removed:", imfile
+	
