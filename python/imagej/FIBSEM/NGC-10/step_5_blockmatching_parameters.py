fa0a4d8c37201c457fee3a076134789b6de7be0a
Peter Hague <pete.hague@gmail.com>
2025-12-08 14:01:33 +0000

    steps

--- python/imagej/FIBSEM/NGC-10/step_5_blockmatching_parameters.py
+++ python/imagej/FIBSEM/NGC-10/step_5_blockmatching_parameters.py
@@ -21,8 +21,8 @@ BMdir = tgtDir + "BM-csv/"
 propertiesBM = {
  'name': name,
  'scale': 1, # Compounds with montaging interim_scale
- 'n_threads': numCPUs(),
- 'roi': [2000, 7080, 18360, 16720],#[421, 990, 4697, 4974],
+ 'n_threads': 16, #numCPUs(),
+ 'roi': [840,5064,19464,18408], #[2000, 7080, 18360, 16720],#[421, 990, 4697, 4974],
         # [section_width / 4,  # To extract SIFT features from e.g., center part only, reducing ops by 4x
          #section_height / 4, # [x, y, width, height] or None.
          #section_width / 2,
@@ -34,11 +34,11 @@ propertiesBM = {
 # Parameters for blockmatching
 paramsBlockMatching = {
  'scale': propertiesBM['scale'], # Compounds with montaging interim_scale, so 0.5 would mean half of that
- 'meshResolution': 10, # 10x10 = 100 points
+ 'meshResolution': 15, # 10x10 = 100 points
  'minR': 0.1, # min PMCC (Pearson product-moment correlation coefficient)
  'rod': 0.9, # max second best r / best r
  'maxCurvature': 1000.0, # default is 10
- 'searchRadius': 200, # Maximum expected displacement between slices after SIFT-based registration.
+ 'searchRadius': 100, # Maximum expected displacement between slices after SIFT-based registration.
                       # Make it large enough, 300 is a good first searcRadius value. 50 to a 100 for a fast run.
  'blockRadius': 200, # small, yet enough: size of the window to use for comparing across images.
 }
@@ -57,7 +57,7 @@ paramsTileConfigurationBM = {
   "maxIterations": 5000, # Optimizer iterations for each chunk of chunk_size sections
   "damp": 1.0, # Saalfeld recommends 1.0, which means no damp
   "nThreadsOptimizer": numCPUs(), # as many as CPU cores
-  "chunk_size": 200, #400, # Will align in 50% overlapping chunks for best use of the optimizer
+  "chunk_size": 400, # Will align in 50% overlapping chunks for best use of the optimizer
   "chunk_maxIterations": 40000, # Iterations for the cross-chunk alignment
   "fixed_tile_index": None, # None implies use the middle tile. Otherwise provide an index (0-based)
 }
