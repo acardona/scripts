775c3a0f027b79a41ae9996a49f2e1e85458beae
Peter Hague <pete.hague@gmail.com>
2025-12-11 10:42:54 +0000

    Whitespace correction

--- python/imagej/FIBSEM/NGC-10/get_trackem.py
+++ python/imagej/FIBSEM/NGC-10/get_trackem.py
@@ -16,19 +16,19 @@ for i in range(len(layers)):
 	start=True
 	outputstring = '"m00","m01","m02","m10","m11","m12"'
 	for patch in Display.getFront().getLayer().getPatches(True):
-	  if start:
-	  	fileroot = "_".join(patch.filePath.split("_")[0:3]) + "_"
-	  	fileroot = fileroot.replace("tmp/", "")
-	  	print fileroot+".csv"
-	  	roottrans = patch.getAffineTransform()
-	  	rootx = roottrans.getTranslateX()
-	  	rooty = roottrans.getTranslateY()
-	  	print "Root tile:",rootx, rooty
-	  	start = False
-	  transform = patch.getAffineTransform()
-	  tx = transform.getTranslateX()
-	  ty = transform.getTranslateY()
-	  outputstring += "\n1.0,0.0,{0},0.0,1.0,{1}".format(tx-rootx, ty-rooty)
+		if start:
+			fileroot = "_".join(patch.filePath.split("_")[0:3]) + "_"
+			fileroot = fileroot.replace("tmp/", "")
+			print fileroot+".csv"
+			roottrans = patch.getAffineTransform()
+			rootx = roottrans.getTranslateX()
+			rooty = roottrans.getTranslateY()
+			print "Root tile:",rootx, rooty
+			start = False
+		transform = patch.getAffineTransform()
+		tx = transform.getTranslateX()
+		ty = transform.getTranslateY()
+		outputstring += "\n1.0,0.0,{0},0.0,1.0,{1}".format(tx-rootx, ty-rooty)
 	print(outputstring)
 	
 	with open(fileroot+".csv", "w") as f:
