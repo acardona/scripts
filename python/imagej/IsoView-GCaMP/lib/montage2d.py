8d4b8a5ad2ce065b79bcee2d4022dbe9b861c13d
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2025-12-12 18:12:07 +0000

    Fix reporting error

--- python/imagej/IsoView-GCaMP/lib/montage2d.py
+++ python/imagej/IsoView-GCaMP/lib/montage2d.py
@@ -1368,7 +1368,7 @@ def runEvaluateMontages(groupNames, tileGroups, csvDir, slice_indices, overlap, 
         futures.append((i, exe.submit(Task(evaluateMontage, groupNames[i-1], tileGroups[i-1], csvDir, overlap, offset, params_pixels, PCscale=PCscale))))
       for i, fu in futures:
         scores = fu.get()
-        syncPrintQ("Montage scores for slice index %i (%s):\n%s" % (i, groupNames[i-1], "\n".join("  %s: %f, %f" % (s, cc, d) for s, (cc, d) in scores)))
+        #syncPrintQ("Montage scores for slice index %i (%s):\n%s" % (i, groupNames[i-1], "\n".join("  %s: %f, %f, %f, %f, %f" % (s, cc, dx, dy, d, cc2) for s, (cc, dx, dy, d, cc2) in scores)))
       # Open the table
       makeMontageEvaluationTable(groupNames, tileGroups, imp, csvDir, show=True)
       #
