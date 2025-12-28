0325cffd2bfd0303958908e13c97a18434762bfc
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2025-12-28 17:11:43 +0000

    FIBSEM NGC-10: export script, which splices first 438 slices from shifts volume.

--- python/imagej/FIBSEM/NGC-10/step_8_export_run.py
+++ python/imagej/FIBSEM/NGC-10/step_8_export_run.py
@@ -22,15 +22,45 @@ from step_7_export_parameters import n5Dir, crop_roi, rotate, paramsN5
 sys.path.append(libDir)
 from lib.serial2Dregistration import loadAlignedImage
 from lib.io import writeN5
+from lib.ui import wrap
+
+from net.imglib2.view import Views
 
 
 # Load the montages in full resolution, unaligned, and cropped as per crop_roi
-img, imp = loadAlignedImage(name, srcDir, repairedDir, montageDir,
+imgShiftBM, impShiftBM = loadAlignedImage(name, srcDir, repairedDir, montageDir,
         SIFTdir, BMdir,
         to_remove, ignore_images, replace_images,
         first_section, last_section, replace_sections,
         section_width, section_height, crop_roi, params_pixels,
         rotate=rotate, preload=paramsN5["block_size"][2])
+
+print "Loaded shift+BM"
+
+# Load the same but without BM
+imgShift, impShift = loadAlignedImage(name, srcDir, repairedDir, montageDir,
+        SIFTdir, "/none/", # purposefully giving a folder that doesn't exist for the BMdir
+        to_remove, ignore_images, replace_images,
+        first_section, last_section, replace_sections,
+        section_width, section_height, crop_roi, params_pixels,
+        rotate=rotate, preload=paramsN5["block_size"][2])
+
+print "Loaded shifts only"
+
+# Splice 0-438 from shifts volume with 439-end from shift+BM volume
+img = Views.concatenate(2, # on the Z axis
+                        Views.interval(imgShift, [0, 0, 0], [imgShift.dimension(0) -1, imgShift.dimension(1) -1, 438]),
+                        Views.interval(imgShiftBM, [0, 0, 439], [imgShiftBM.dimension(0) -1, imgShiftBM.dimension(1) -1, imgShiftBM.dimension(2) -1]))
+
+print "Spliced."
+
+# View the spliced volume
+wrap(img, title="spliced").show()
+
+# To export when the image is showing, comment out the above and uncomment this below:
+#from ij import IJ
+#img = IJ.getImage().getStack().getSource().getSource().getSource() # IntervalView (4 dim), MixedTransformView (4 dim), StackView (3 dim)
+#print img
 
 # Write N5 volume
 writeN5(img, n5Dir, name,
