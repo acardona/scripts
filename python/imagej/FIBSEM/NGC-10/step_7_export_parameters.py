4ca8e4fa4cc640f5ec7bb7e6317e33a675b891f4
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2025-12-28 17:09:46 +0000

    FIBSEM NGC-10: exporting from fibserver1 so only 32 Z block size

--- python/imagej/FIBSEM/NGC-10/step_7_export_parameters.py
+++ python/imagej/FIBSEM/NGC-10/step_7_export_parameters.py
@@ -4,7 +4,7 @@ import sys, os
 # Add current directory to path
 sys.path.append(os.path.dirname(sys.argv[0]))
 # Import parameters used for montaging
-from step_1_montage_parameters import libDir, name, tgtDir, section_width, section_height
+from step_1_montage_parameters import libDir, name, tgtDir, section_width, section_height, params_pixels
 # Import registration library functions
 sys.path.append(libDir)
 from lib.util import numCPUs
@@ -15,17 +15,17 @@ from lib.util import numCPUs
 n5Dir = tgtDir + "n5/"
 
 # Region of interest in 2D for exporting
-crop_roi = [0, # X
-            0, # Y
-            section_width,  # width
-            section_height] # height
+crop_roi = [912, 1416, 18648, 17904] #[0, # X
+            #0, # Y
+            #section_width,  # width
+            #section_height] # height
 
 # Rotate the view: None, "right", "left", or "180"
 rotate = None
 
 # Parameters on what to export
 paramsN5 = {
-  "block_size": [256, 256, 64], # e.g., [128,128,128]
+  "block_size": [256, 256, 32], # e.g., [128,128,128]
   "gzip_compression": 4, # between 0 (no compression) and 9
-  "n_threads": numCPUs(), # for writing
+  "n_threads": numCPUs() - 2, # for writing
 }
