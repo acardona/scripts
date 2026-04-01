349548f04567cb40f6beb6af603b246df0d4729a
Albert Cardona <acardona@mrc-lmb.cam.ac.uk>
2026-04-01 14:25:53 +0000

    FIBSEM YY9_Gaba: script to duplicate stacks in parallel.

---
+++ python/imagej/FIBSEM/YY9_Gaba/view_section_ranges.py
@@ -0,0 +1,16 @@
+# Duplicate a stack in parallel
+import sys, os
+sys.path.append("/lmb/home/acardona/lab/scripts/python/imagej/IsoView-GCaMP/")
+from lib.ui import duplicateInParallel
+from ij import IJ
+
+
+imp = IJ.getImage()
+roi = imp.getRoi()
+
+ranges = [
+  (0, 1850),
+]
+
+for r in ranges:
+  copy = duplicateInParallel(imp, range(*r), n_threads=256, shallow=True, show=True, roi=roi)
